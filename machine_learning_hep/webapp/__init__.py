8d9757ce984dfc9ee377a688658bce211ce16fd7
Gian Michele Innocenti <gminnocen@gmail.com>
2019-01-08 16:58:13 +0100

    web app taking parameters from database

--- machine_learning_hep/webapp/__init__.py
+++ machine_learning_hep/webapp/__init__.py
@@ -28,7 +28,9 @@ from flask import send_file
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt # pylint: disable=wrong-import-position
-from machine_learning_hep.functions import vardistplot # pylint: disable=wrong-import-position
+from machine_learning_hep.functions import do_correlation, create_mlsamples # pylint: disable=wrong-import-position
+from machine_learning_hep.general import get_database_ml_parameters, getdataframe # pylint: disable=wrong-import-position
+
 
 
 APP = Flask(__name__)
@@ -43,26 +45,58 @@ def root():
 def post_form():
   # request.args {'slct1': 'JetTagging', 'slct2': 'hypertritium'}
 #   file = request.form.files[
-    slct1 = request.form['slct1']
-    slct2 = request.form['slct2']
-    responseString = '{0},{1}'.format(slct1, slct2)
-    data = [random.randint(1, 100) for _ in range(100)]
-    file = uproot.open("/Users/gianmicheleinnocenti/Desktop/MLPackage/MachineLearningHEP/"
-                       "machine_learning_hep/data/inputroot/"
-                       "AnalysisResults_Lambdac_Data_CandBased_skimmed.root")
-    tree = file["fTreeLcFlagged"]
-    mylistvariables = ["inv_mass_ML", "pt_cand_ML", "d_len_ML", "d_len_xy_ML"]
-    df = tree.pandas.df(mylistvariables)
-    data = df.inv_mass_ML
-    plt.figure(figsize=(15, 15))
-    plt.hist(data)
-#     imageIO_vardist, imageIO_scatterplot, imageIO_scatterplot, imageIO_scatterplot \
-    imageIO_vardist = vardistplot(df, df, mylistvariables, "./")
 
-    pngData = binascii.b2a_base64(imageIO_vardist.read())
-    print(pngData.decode("utf-8"))
+    mlsubtype = request.form['slct1']
+    case = request.form['slct2']
+    data = get_database_ml_parameters()
+    filesig, filebkg = data[case]["sig_bkg_files"]
+    trename = data[case]["tree_name"]
+    var_all = data[case]["var_all"]
+    var_signal = data[case]["var_signal"]
+    sel_signal = data[case]["sel_signal"]
+    sel_bkg = data[case]["sel_bkg"]
+    var_training = data[case]["var_training"]
+    var_corr_x, var_corr_y = data[case]["var_correlation"]
+
+#     filedata, filemc = data[case]["data_mc_files"]
+#     var_target = data[case]["var_target"]
+#     var_boundaries = data[case]["var_boundaries"]
+
+    responseString = '{0},{1}'.format(mlsubtype, case)
+
+    filesig = "../"+filesig
+    filebkg = "../"+filebkg
+    var_skimming = ["pt_cand_ML"]
+    varmin = [0]
+    varmax = [10]
+    rnd_shuffle = 12
+    nevt_sig = 1000
+    nevt_bkg = 1000
+    test_frac = 0.2
+    rnd_splt = 12
+    plotdir = "./"
+
+    df_sig = getdataframe(filesig, trename, var_all)
+    df_bkg = getdataframe(filebkg, trename, var_all)
+    # pylint: disable=unused-variable
+    _, _, df_sig_train, df_bkg_train, _, _, \
+    x_train, y_train, x_test, y_test = \
+        create_mlsamples(df_sig, df_bkg, sel_signal, sel_bkg, rnd_shuffle,
+                         var_skimming, varmin, varmax, var_signal, var_training,
+                         nevt_sig, nevt_bkg, test_frac, rnd_splt)
+    imageIO_vardist, imageIO_scatterplot, imageIO_corr_sig, imageIO_corr_bkg = \
+        do_correlation(df_sig_train, df_bkg_train, var_all, var_corr_x, var_corr_y, plotdir)
+
+    imageIO_vardist = binascii.b2a_base64(imageIO_vardist.read())
+    imageIO_scatterplot = binascii.b2a_base64(imageIO_scatterplot.read())
+    imageIO_corr_sig = binascii.b2a_base64(imageIO_corr_sig.read())
+    imageIO_corr_bkg = binascii.b2a_base64(imageIO_corr_bkg.read())
+#     print(pngData.decode("utf-8"))
     return render_template('display.html', responseString=responseString,
-                           plotBase64=pngData.decode("utf-8"))
+                           imageIO_vardist=imageIO_vardist.decode("utf-8"), \
+                           imageIO_scatterplot=imageIO_scatterplot.decode("utf-8"), \
+                           imageIO_corr_sig=imageIO_corr_sig.decode("utf-8"), \
+                           imageIO_corr_bkg=imageIO_corr_bkg.decode("utf-8"))
 
 def main():
     # This is used when running locally only. When deploying to Google App
