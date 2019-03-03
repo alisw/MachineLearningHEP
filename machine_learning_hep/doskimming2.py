43813128fde365a078f89fa24b472861ca5c788e
Gian Michele Innocenti <ginnocen@aliceml.cern.ch>
2019-03-03 20:49:52 +0000

    Split the flattening and the merging

--- machine_learning_hep/doskimming2.py
+++ machine_learning_hep/doskimming2.py
@@ -21,48 +21,27 @@ main macro for charm analysis with python
 
 # pylint: disable=import-error
 
-import os
-import time
 import multiprocessing as mp
-import pandas as pd
 import uproot
 from machine_learning_hep.general import get_database_ml_parameters
-#from machine_learning_hep.root import write_tree
-from machine_learning_hep.listfiles import list_files
-
-def flattenroot_to_pandas(namefileinput, treenamein, var_all, skimming_sel):
-    tree = uproot.open(namefileinput)[treenamein]
-    df = tree.pandas.df(branches=var_all, flatten=True)
-    df = df.query(skimming_sel)
-    return df
+from machine_learning_hep.listfiles import list_files_dir
 
 def writelist_tofile(fileout, mylist):
     with open(fileout, 'w') as f:
         for item in mylist:
             f.write("%s\n" % item)
 
-def merger_skimmer(listfiles, index, directory, case, treenamein, \
-                   var_all, skimming_sel, nmaxfile):
-    #dfList = []
-    df = flattenroot_to_pandas(listfiles, treenamein, var_all, skimming_sel)
-    fileoutpkl = "%s/AnalysisResults%d.pkl" % (directory, index)
-    df.to_pickle(fileoutpkl)
-    #for namefileinput in listfiles:
-    #    df = flattenroot_to_pandas(namefileinput, treenamein, var_all, skimming_sel)
-    #    df.to_pickle(namefileinput.replace(".root", ".pkl"))
-    #filemergedpkl = "%s/AnalysisResultsMerged%sIndex%d_Nfiles%d.pkl" % \
-    #                (directory, case, index, nmaxfile)
-    #filelist = "%s/mergedfilesMerged%sIndex%d_Nfiles%d.txt" % (directory, case, index, nmaxfile)
-    #writelist_tofile(filelist, listfiles)
-    #dfTot = pd.concat(dfList)
-    #dfTot.to_pickle(filemergedpkl)
+def flattenroot_to_pandas(filein, fileout, treenamein, var_all, skimming_sel):
+    print(filein)
+    tree = uproot.open(filein)[treenamein]
+    df = tree.pandas.df(branches=var_all, flatten=True)
+    df = df.query(skimming_sel)
+    df.to_pickle(fileout)
 
-def mergefiles(chunks, mergeddir, case, treenamein, \
-               var_all_unflat, skimming_sel, nmaxfile):
-    processes = [mp.Process(target=merger_skimmer, args=(mylist, index, mergeddir, case, \
-                                                         treenamein, var_all_unflat, \
-                                                         skimming_sel, nmaxfile))
-                 for index, mylist in enumerate(chunks)]
+def flattenallpickle(chunck, chunckout, treenamein, var_all, skimming_sel):
+    processes = [mp.Process(target=flattenroot_to_pandas, args=(filein, chunckout[index], \
+                                                         treenamein, var_all, skimming_sel))
+                 for index, filein in enumerate(chunck)]
     for p in processes:
         p.start()
     for p in processes:
@@ -73,26 +52,28 @@ def doskimming2():
 
     case = "Dzero"
     namefileinput = 'AnalysisResults.root'
+    namefileinputpkl = 'AnalysisResults%s.pkl' % case
     treenamein = 'PWGHF_TreeCreator/tree_D0'
     data = get_database_ml_parameters()
     var_all_unflat = data[case]["var_all_unflat"]
     skimming_sel = "n_cand> 0 & pt_cand>3"
-    nmaxfile = 50
-    nmaxfilestoprocess = 500
-    tstart = time.time()
+    nmaxchunks = 50
+    nmaxfiles = 50
 
-    inputdir = "/home/ginnocen/LearningPythonML/inputs"
+    #inputdir = "/home/ginnocen/LearningPythonML/inputs"
     inputdir = "/data/HeavyFlavour/DmesonsLc_pp_5TeV/Data_LHC17pq/12-02-2019/340_20190211-2126/unmerged" # pylint: disable=line-too-long
-    mergeddir = "/data/HeavyFlavour/DmesonsLc_pp_5TeV/Data_LHC17pq/12-02-2019/340_20190211-2126/mergedPython" # pylint: disable=line-too-long
+    mergeddir = \
+    "/data/HeavyFlavour/DmesonsLc_pp_5TeV/Data_LHC17pq/12-02-2019/340_20190211-2126/unmergedpkl" # pylint: disable=line-too-long
 
-    listfilespath, _ = list_files(inputdir, outdir="", \
-                                  filenameinput=namefileinput, filenameoutput="")
-    listfilespath = listfilespath[:nmaxfilestoprocess]
+    listfilespath, listfilespathout = list_files_dir(inputdir, outdir=mergeddir, \
+                                  filenameinput=namefileinput, filenameoutput=namefileinputpkl)
+    listfilespath = listfilespath[:nmaxfiles]
+    listfilespathout = listfilespathout[:nmaxfiles]
     print(len(listfilespath))
-    chunks = [listfilespath[x:x+nmaxfile] for x in range(0, len(listfilespath), nmaxfile)]
-    for i, chunk in enumerate(chunks):
-        print("chunk number =", i)
-        mergefiles(chunk, mergeddir, case, treenamein, var_all_unflat, skimming_sel, nmaxfile)
-    tstop = time.time()
-    print("total time =", tstop - tstart)
+    print(len(listfilespathout))
+    chunks = [listfilespath[x:x+nmaxchunks] for x in range(0, len(listfilespath), nmaxchunks)]
+    chunksout = [listfilespathout[x:x+nmaxchunks] for x in range(0, len(listfilespathout), nmaxchunks)]
+    for chunck, chunckout in zip(chunks, chunksout):
+        flattenallpickle(chunck, chunckout, treenamein, var_all_unflat, skimming_sel)
+
 doskimming2()
