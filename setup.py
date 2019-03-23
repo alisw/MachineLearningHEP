007f04aefb9b45dd4df0d27266e256617a699751
Gian Michele Innocenti <ginnocen@aliceml.cern.ch>
2019-03-23 23:44:33 +0000

    Add missing root_numpy

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
   # https://packaging.python.org/en/latest/requirements.html
   install_requires=[ "numpy", "pandas", "scipy", "matplotlib", "seaborn", "uproot", "scikit-learn",
                      "xgboost", "keras", "tensorflow", "PyYaml", "pylint", "twisted", "klein",
-                    "Jinja2", "numba", "pyarrow", "root_numpy"],
+                    "Jinja2", "numba", "pyarrow"],
 
   python_requires='>=3.6, <3.7',
 
