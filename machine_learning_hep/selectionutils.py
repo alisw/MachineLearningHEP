faeb373122369cdfff1fa1eff997d51fc0e24bca
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-04-03 17:40:00 +0200

    Add possibility to apply linear cuts from config file instead of std selection bit

--- machine_learning_hep/selectionutils.py
+++ machine_learning_hep/selectionutils.py
@@ -13,7 +13,7 @@
 #############################################################################
 
 """
-utilities for fiducial acceptance and pid selections
+utilities for fiducial acceptance, pid, single topological variable selections and normalization
 """
 
 import numba
@@ -120,6 +120,21 @@ def selectpid_lctov0bachelor(array_nsigma_tpc, array_nsigma_tof, nsigmacut):
         else:
             array_is_pid_sel.append(False)
     return array_is_pid_sel
+
+@numba.njit
+def selectcand_lincut(array_cut_var, minvalue, maxvalue, isabs):
+    array_is_sel = []
+
+    for icand in range(array_cut_var):
+        if isabs:
+            value = abs(array_cut_var[icand])
+        else:
+            value = array_cut_var[icand]
+        if value >= minvalue & value <= maxvalue:
+            array_is_sel.append(True)
+        else:
+            array_is_sel.append(False)
+    return array_is_sel
 
 def getnormforselevt(df_evt):
     #accepted events
