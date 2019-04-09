04563d2b7a0a247ca052ee5abce962d8db7c8082
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-04-09 08:41:17 +0000

    Fix pylint

--- machine_learning_hep/do_get_normalisation.py
+++ machine_learning_hep/do_get_normalisation.py
@@ -12,13 +12,14 @@
 ##   along with this program. if not, see <https://www.gnu.org/licenses/>. ##
 #############################################################################
 
-import pandas as pd
 import os
 import yaml
+import pandas as pd
 
-from ROOT import TFile, TH1F, TCanvas # pylint: disable=import-error, no-name-in-module
+from ROOT import TFile, TH1F # pylint: disable=import-error, no-name-in-module
 from machine_learning_hep.selectionutils import getnormforselevt
 
+# pylint: disable=invalid-name
 case = "Dspp5TeV"
 
 with open("data/database_ml_parameters.yml", 'r') as param_config:
@@ -32,13 +33,12 @@ df_evt_all = pd.read_pickle(os.path.join(folder, namefile_evt))
 nselevt = len(df_evt_all.query("is_ev_rej==0"))
 norm = getnormforselevt(df_evt_all)
 
-hNorm = TH1F("hEvForNorm",";;Normalisation",2,0.5,2.5)
-hNorm.GetXaxis().SetBinLabel(1,"normsalisation factor")
-hNorm.GetXaxis().SetBinLabel(2,"selected events")
-hNorm.SetBinContent(1,norm)
-hNorm.SetBinContent(2,nselevt)
+hNorm = TH1F("hEvForNorm", ";;Normalisation", 2, 0.5, 2.5)
+hNorm.GetXaxis().SetBinLabel(1, "normsalisation factor")
+hNorm.GetXaxis().SetBinLabel(2, "selected events")
+hNorm.SetBinContent(1, norm)
+hNorm.SetBinContent(2, nselevt)
 
-outfile = TFile("Normalisation_%s.root" % case,"recreate")
+outfile = TFile("Normalisation_%s.root" % case, "recreate")
 hNorm.Write()
 outfile.Close()
-
