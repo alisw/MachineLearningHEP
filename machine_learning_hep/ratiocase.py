ae232d405f85f045d12f212a32853d42a538eb36
Gian Michele Innocenti <ginnocen@cern.ch>
2019-08-18 22:21:38 +0000

    New macro for ratio comparison

--- machine_learning_hep/ratiocase.py
+++ machine_learning_hep/ratiocase.py
@@ -26,7 +26,7 @@ from ROOT import TStyle
 
 # pylint: disable=import-error, no-name-in-module, unused-import
 # pylint: disable=too-many-statements
-def ratiocase(imult, case_num, typean_num, case_den, typean_den):
+def ratiocase(case_num, case_den, typean):
 
     gROOT.SetStyle("Plain")
     gStyle.SetOptStat(0)
@@ -36,50 +36,50 @@ def ratiocase(imult, case_num, typean_num, case_den, typean_den):
     gStyle.SetFrameFillColor(0)
     gStyle.SetOptTitle(0)
 
-    ccross = TCanvas('cCross', 'The Fit Canvas', 100, 600)
-
     with open("data/database_ml_parameters_%s.yml" % case_num, 'r') as param_config_num:
         data_param_num = yaml.load(param_config_num, Loader=yaml.FullLoader)
 
     with open("data/database_ml_parameters_%s.yml" % case_den, 'r') as param_config_den:
         data_param_den = yaml.load(param_config_den, Loader=yaml.FullLoader)
 
-    folder_num = data_param_num[case_num]["analysis"][typean_num]["data"]["resultsallp"]
-    folder_den = data_param_den[case_den]["analysis"][typean_den]["data"]["resultsallp"]
+    folder_num = data_param_num[case_num]["analysis"][typean]["data"]["resultsallp"]
+    folder_den = data_param_den[case_den]["analysis"][typean]["data"]["resultsallp"]
+    binsmin_num = data_param_num[case_num]["analysis"][typean]["sel_binmin2"]
+    binsmax_num = data_param_num[case_num]["analysis"][typean]["sel_binmax2"]
+    latexbin2var = data_param_num[case_num]["analysis"][typean]["latexbin2var"]
 
-    binsmin_num = data_param_num[case_num]["analysis"][typean_num]["sel_binmin2"]
-    binsmax_num = data_param_num[case_num]["analysis"][typean_num]["sel_binmax2"]
-
-    binmin = binsmin_num[imult]
-    binmax = binsmax_num[imult]
-
-    file_num = TFile.Open("%s/finalcross%s%s.root" % (folder_num, case_num, typean_num))
-    file_den = TFile.Open("%s/finalcross%s%s.root" % (folder_den, case_den, typean_den))
-    print("ratiocase")
+    ccross = TCanvas('cCross', 'The Fit Canvas', 100, 600)
     ccross = TCanvas('cCross', 'The Fit Canvas')
     ccross.SetCanvasSize(1500, 1500)
     ccross.SetWindowSize(500, 500)
     ccross.SetLogx()
-    hcross_num = file_num.Get("hcross%d" % (imult))
-    hcross_den = file_den.Get("hcross%d" % (imult))
-    hcross_num.Divide(hcross_den)
-    hcross_num.GetXaxis().SetTitle("p_{T} (GeV)")
-    hcross_num.GetYaxis().SetTitle("Particle ratio")
-    hcross_num.GetYaxis().SetRangeUser(0., 1.)
-    hcross_num.Draw()
-    ccross.SaveAs("ComparisonRatios%.2f_%.2f_%s%s_%s%s.eps" % \
-                  (binmin, binmax, case_num, typean_num, case_den, typean_den))
 
+    legyield = TLegend(.3, .65, .7, .85)
+    legyield.SetBorderSize(0)
+    legyield.SetFillColor(0)
+    legyield.SetFillStyle(0)
+    legyield.SetTextFont(42)
+    legyield.SetTextSize(0.035)
 
-ratiocase(0, "LcpK0spp", "MBvspt", "D0pp", "MBvspt")
-ratiocase(1, "LcpK0spp", "MBvspt", "D0pp", "MBvspt")
-ratiocase(2, "LcpK0spp", "MBvspt", "D0pp", "MBvspt")
-ratiocase(3, "LcpK0spp", "MBvspt", "D0pp", "MBvspt")
+    file_num = TFile.Open("%s/finalcross%s%s.root" % (folder_num, case_num, typean))
+    file_den = TFile.Open("%s/finalcross%s%s.root" % (folder_den, case_den, typean))
 
-ratiocase(0, "LcpK0spp", "SPDvspt", "D0pp", "SPDvspt")
-ratiocase(1, "LcpK0spp", "SPDvspt", "D0pp", "SPDvspt")
-ratiocase(2, "LcpK0spp", "SPDvspt", "D0pp", "SPDvspt")
+    for imult, _ in enumerate(binsmax_num):
+        print(imult)
+        hratio = file_num.Get("hcross%d" % (imult))
+        hcross_den = file_den.Get("hcross%d" % (imult))
+        hratio.Divide(hcross_den)
+        hratio.GetXaxis().SetTitle("p_{T} (GeV)")
+        hratio.GetYaxis().SetTitle("Particle ratio")
+        hratio.GetYaxis().SetRangeUser(0., 1.)
+        hratio.Draw("same")
+        legyieldstring = "%.1f < %s < %.1f" % \
+                (binsmin_num[imult], latexbin2var, binsmax_num[imult])
+        legyield.AddEntry(hratio, legyieldstring, "LEP")
+    legyield.Draw()
+    ccross.SaveAs("ComparisonRatios_%s%s_%s.eps" % \
+                  (case_num, case_den, typean))
 
-ratiocase(0, "LcpK0spp", "V0mvspt", "D0pp", "V0mvspt")
-ratiocase(1, "LcpK0spp", "V0mvspt", "D0pp", "V0mvspt")
-ratiocase(2, "LcpK0spp", "V0mvspt", "D0pp", "V0mvspt")
+ratiocase("LcpK0spp", "D0pp", "MBvspt")
+ratiocase("LcpK0spp", "D0pp", "SPDvspt")
+ratiocase("LcpK0spp", "D0pp", "V0mvspt")
