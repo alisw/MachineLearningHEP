9a9e60a8669eaefe73e50b9ccace6e1d4d4a56c6
Gian Michele Innocenti <gminnocen@gmail.com>
2019-09-23 18:55:35 +0200

    Disable weights for trigger + update ana.sh (#324)
    * Disable weights for trigger for the moment
    
    * Update ana.sh with recent analysis types

--- machine_learning_hep/ana.sh
+++ machine_learning_hep/ana.sh
@@ -15,31 +15,18 @@ export JOBIDX
 
 unset DISPLAY
 export MLPBACKEND=pdf
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt_v0m
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt_ntrkl
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a SPDvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a V0Mvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBjetvspt
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt_ntrkl &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt_v0m &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a MBvspt_perc &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a SPDvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a V0mvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_D0pp.yml -a V0mvspt_perc_v0m &
 
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a MBvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a MBvspt_v0m
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a MBvspt_ntrkl
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a SPDvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a V0Mvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a MBjetvspt
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt_ntrkl &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt_v0m &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt_perc &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a SPDvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a V0mvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a V0mvspt_perc_v0m &
 
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt_v0m
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBvspt_ntrkl
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a SPDvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a V0Mvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpK0spp.yml -a MBjetvspt
-
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt_v0m
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt_ntrkl
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a SPDvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a V0Mvspt
-srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBjetvspt
 wait
