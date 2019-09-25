f54929d5dadce56318a97d1487a328275b557f29
Gian Michele Innocenti <ginnocen@cern.ch>
2019-09-25 21:19:24 +0200

    Fix multi unfolding

--- machine_learning_hep/multianalyzer.py
+++ machine_learning_hep/multianalyzer.py
@@ -67,6 +67,18 @@ class MultiAnalyzer: # pylint: disable=too-many-instance-attributes, too-many-st
                 self.process_listsample[indexp].feeddown()
         self.myanalyzertot.feeddown()
 
+    def multi_unfolding(self):
+        if self.doperiodbyperiod is True:
+            for indexp in range(self.prodnumber):
+                self.process_listsample[indexp].unfolding()
+        self.myanalyzertot.unfolding()
+
+    def multi_unfolding_closure(self):
+        if self.doperiodbyperiod is True:
+            for indexp in range(self.prodnumber):
+                self.process_listsample[indexp].unfolding_closure()
+        self.myanalyzertot.unfolding_closure()
+
     def multi_side_band_sub(self):
         if self.doperiodbyperiod is True:
             for indexp in range(self.prodnumber):
