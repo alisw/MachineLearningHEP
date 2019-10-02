689d151a4a8a382fe20d9900cbe3fbd4e6c900b9
Luuk Vermunt <luuk.vermunt@cern.ch>
2019-10-02 15:23:23 +0200

    Store plots in folder in DerivedResults

--- machine_learning_hep/ana.sh
+++ machine_learning_hep/ana.sh
@@ -36,4 +36,14 @@ srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_paramet
 srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a V0mvspt &
 srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_Dspp.yml -a V0mvspt_perc_v0m &
 
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt_ntrkl &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt_v0m &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a MBvspt_perc &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a SPDvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a V0mvspt &
+srun python do_entire_analysis.py -r default_ana.yml -d data/database_ml_parameters_LcpKpipp.yml -a V0mvspt_perc_v0m &
+
 wait
+
+srun python plot_hfmassfitter.py
+srun python plot_hfptspectrum.py
