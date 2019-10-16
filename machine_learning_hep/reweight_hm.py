73959d0c47b26a23e8d43325fc08280450aa9dd6
Gian Michele Innocenti <ginnocen@cern.ch>
2019-10-17 00:33:11 +0200

    Fix pylint

--- machine_learning_hep/reweight_hm.py
+++ machine_learning_hep/reweight_hm.py
@@ -15,35 +15,32 @@
 """
 main script for doing final stage analysis
 """
-import os
 # pylint: disable=import-error, no-name-in-module, unused-import
 import yaml
 from ROOT import gROOT, TFile, TCanvas, TF1, TH1F
 from machine_learning_hep.utilities import plot_histograms
 
-filespd = TFile.Open("/data/DerivedVal/mcvalspdhm_18/AnalysisResultsROOTEvtVal.root", "read")
-filemb = TFile.Open("/data/DerivedVal/dataval_18/AnalysisResultsROOTEvtVal.root", "read")
-
-
-histospd = filespd.Get("hbitINT7vsn_tracklets_corr")
-histomb = filemb.Get("hbitINT7vsn_tracklets_corr")
-hratio = histomb.Clone("hratio")
-hratio.Divide(histospd)
-hweight = TH1F("Weights0", "Weights0", 200, -0.5, 199.5)
-c = TCanvas("c", "c", 1000, 1000)
-c.cd()
-hratio.GetXaxis().SetRangeUser(30,100)
-func = TF1("func", "[0]+x*[1]+x*x*[2]+x*x*x*[3]", 35, 100)
-hratio.Fit(func, "L", "", 35, 100)
-hratio.Draw()
-for ibin in range(hweight.GetNbinsX()):
-    bincenter = hweight.GetBinCenter(ibin + 1)
-    hweight.SetBinContent(ibin+1, func.Eval(bincenter))
-    hweight.SetBinError(ibin+1, 0.)
-
-c.SaveAs("canvasDs.pdf")
-f = TFile("reweighting/prodDs_spdhm/mcweights.root", "recreate")
-f.cd()
-hratio.Write()
-hweight.Write()
-f.Close()
+def make_syst_bin_edges():
+    filespd = TFile.Open("/data/DerivedVal/mcvalspdhm_18/AnalysisResultsROOTEvtVal.root", "read")
+    filemb = TFile.Open("/data/DerivedVal/dataval_18/AnalysisResultsROOTEvtVal.root", "read")
+    histospd = filespd.Get("hbitINT7vsn_tracklets_corr")
+    histomb = filemb.Get("hbitINT7vsn_tracklets_corr")
+    hratio = histomb.Clone("hratio")
+    hratio.Divide(histospd)
+    hweight = TH1F("Weights0", "Weights0", 200, -0.5, 199.5)
+    c = TCanvas("c", "c", 1000, 1000)
+    c.cd()
+    hratio.GetXaxis().SetRangeUser(30, 100)
+    func = TF1("func", "[0]+x*[1]+x*x*[2]+x*x*x*[3]", 35, 100)
+    hratio.Fit(func, "L", "", 35, 100)
+    hratio.Draw()
+    for ibin in range(hweight.GetNbinsX()):
+        bincenter = hweight.GetBinCenter(ibin + 1)
+        hweight.SetBinContent(ibin+1, func.Eval(bincenter))
+        hweight.SetBinError(ibin+1, 0.)
+    c.SaveAs("canvasDs.pdf")
+    f = TFile("reweighting/prodDs_spdhm/mcweights.root", "recreate")
+    f.cd()
+    hratio.Write()
+    hweight.Write()
+    f.Close()
