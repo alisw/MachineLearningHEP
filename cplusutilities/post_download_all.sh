94b20de25d1cdd620b9923adc396b7e856cfb77f
vkucera <vit.kucera@cern.ch>
2020-02-04 22:32:10 +0100

    Add a script for running post_download for all unmerged directories. (#527)

---
+++ cplusutilities/post_download_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+path="$1"
+if [ ! -d "$path" ]
+then
+  echo "Input path $path does not exist!"
+  exit 1
+fi
+
+for dir in $(find $path -type d -name unmerged)
+do
+  dirout=${dir/unmerged/merged}
+  if [ -d "$dirout" ]
+  then
+    echo "Output directory $dirout already exists. Skipping"
+    continue
+  fi
+  ./post_download.sh --input ${dir/unmerged/} --target-size 500000 --jobs 50
+done
+
+exit 0
+
