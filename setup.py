46df34f4e2e9fcc35a2192da6bda56c83dcee66c
benedikt-voelkel <benedikt.voelkel@gmx.de>
2020-02-07 16:49:00 +0100

    fix versions in setup.py (#535)
    Fix version of dependencies to ensure reproducibility.

--- setup.py
+++ setup.py
@@ -69,9 +69,11 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=[ "numpy", "pandas", "scipy", "matplotlib", "seaborn", "uproot", "scikit-learn",
-                     "xgboost==0.90", "keras", "tensorflow", "PyYaml", "pylint==2.4.3", "twisted", "klein",
-                    "Jinja2>=2.10.1", "numba", "pyarrow", "lz4>=2.1.10", "pyyaml"],
+  install_requires=[ "numpy==1.17.4", "pandas==0.24.2", "scipy==1.2.1", "matplotlib==3.0.3",
+                     "seaborn==0.9.0", "uproot==3.4.18", "scikit-learn==0.20.3", "xgboost==0.90",
+                     "keras==2.2.4", "tensorflow==1.13.1", "PyYaml==5.1", "pylint==2.4.3",
+                     "twisted==19.2.0", "klein==17.10.0", "Jinja2==2.10.3", "numba==0.44.0",
+                     "pyarrow==0.13.0", "lz4==2.1.10"],
 
   python_requires='>=3.6, <3.7',
 
