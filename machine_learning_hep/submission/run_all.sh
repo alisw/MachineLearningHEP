9693f0d4900fde5b699bd5e20a89f5b498fb0834
Vit Kucera <vit.kucera@cern.ch>
2020-02-12 19:08:02 +0100

    Add analysis type as mandatory argument.

--- machine_learning_hep/submission/run_all.sh
+++ machine_learning_hep/submission/run_all.sh
@@ -10,8 +10,10 @@ function die
 }
 
 CASE=${1}
+ANA_TYPE=${2}
 
+unset DISPLAY
 export MLPBACKEND=pdf
-srun python do_entire_analysis.py -r default_pre.yml -d ../data/database_ml_parameters_${CASE}.yml
-srun python do_entire_analysis.py -r default_train.yml -d ../data/database_ml_parameters_${CASE}.yml
-srun python do_entire_analysis.py -r default_apply.yml -d ../data/database_ml_parameters_${CASE}.yml
+srun python do_entire_analysis.py -r default_pre.yml -d ../data/database_ml_parameters_${CASE}.yml -a ${ANA_TYPE}
+srun python do_entire_analysis.py -r default_train.yml -d ../data/database_ml_parameters_${CASE}.yml -a ${ANA_TYPE}
+srun python do_entire_analysis.py -r default_apply.yml -d ../data/database_ml_parameters_${CASE}.yml -a ${ANA_TYPE}
