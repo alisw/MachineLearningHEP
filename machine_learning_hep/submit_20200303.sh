06bf3dd039d02867316d62d96d32263c288a9d8c
Gian Michele Innocenti <ginnocen@cern.ch>
2020-03-04 22:33:59 +0100

    Use train test, realistic production

--- machine_learning_hep/submit_20200303.sh
+++ machine_learning_hep/submit_20200303.sh
@@ -30,6 +30,12 @@ rm -rf  /data/DerivedResultsJets/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2016_da
 rm -rf  /data/DerivedResultsJets/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2017_data/351_20200202-0239/resultsMBjetvspt
 rm -rf  /data/DerivedResultsJets/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2018_data/352_20200202-0239/resultsMBjetvspt
 rm -rf  /data/DerivedResultsJets/LckINT7withJets/vAN-20200201_ROOT6-1/pp_data/resultsMBjetvspt
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2016_mc_prodD2H/353_20200201-1929/pklskdec
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2017_mc_prodD2H/353_20200201-1929/pklskdec
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2018_mc_prodD2H/353_20200201-1929/pklskdec
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2016_data/350_20200201-1926/pklskdec
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2017_data/351_20200202-0239/pklskdec
+rm -rf  /data/Derived/LckINT7withJets/vAN-20200201_ROOT6-1/pp_2018_data/352_20200202-0239/pklskdec
 
 
 DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yaml -d data/FFLc/database_ml_parameters_LcpK0spp_20200301.yml
