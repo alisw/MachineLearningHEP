8a90ed8d61eccb732834f986501de298a500878c
Vit Kucera <vit.kucera@cern.ch>
2020-03-07 17:07:01 +0100

    Update paths in the submit scripts.

--- machine_learning_hep/submit_0201.sh
+++ machine_learning_hep/submit_0201.sh
@@ -1,2 +1,2 @@
-DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/database_ml_parameters_LcpK0spp_test_0201_D2H.yml
+DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/JetAnalysis/database_ml_parameters_LcpK0spp_test_0201_D2H.yml
 
