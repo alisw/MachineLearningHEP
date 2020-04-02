ad534e3e64e84ba89d916016067c3c78e46a996d
Gian Michele Innocenti <ginnocen@cern.ch>
2020-04-02 15:50:13 +0200

    Add validation plots

--- machine_learning_hep/processerdhadrons_mult.py
+++ machine_learning_hep/processerdhadrons_mult.py
@@ -31,6 +31,7 @@ from machine_learning_hep.utilities import get_timestamp_string
 #from machine_learning_hep.globalfitter import fitter
 from machine_learning_hep.processer import Processer
 from machine_learning_hep.bitwise import filter_bit_df, tag_bit_df
+from machine_learning_hep.utilities_validation import fillvalidationvsmult
 
 class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-attributes, invalid-name
     # Class Attribute
@@ -56,7 +57,6 @@ class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-att
         self.l_selml = ["y_test_prob%s>%s" % (self.p_modelname, self.lpt_probcutfin[ipt]) \
                        for ipt in range(self.p_nptbins)]
         self.s_presel_gen_eff = datap["analysis"][self.typean]['presel_gen_eff']
-
         self.lvar2_binmin = datap["analysis"][self.typean]["sel_binmin2"]
         self.lvar2_binmax = datap["analysis"][self.typean]["sel_binmax2"]
         self.v_var2_binning = datap["analysis"][self.typean]["var_binning2"]
@@ -88,6 +88,7 @@ class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-att
         self.nbinshisto = datap["analysis"][self.typean]["nbinshisto"]
         self.minvaluehisto = datap["analysis"][self.typean]["minvaluehisto"]
         self.maxvaluehisto = datap["analysis"][self.typean]["maxvaluehisto"]
+        self.mass = datap["mass"]
 
     def gethistonormforselevt_mult(self, df_evt, dfevtevtsel, label, var, weightfunc=None):
 
@@ -121,16 +122,39 @@ class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-att
             fill_hist(hVtxOutMult, df_bit_zvtx_gr10[var])
 
         return hSelMult, hNoVtxMult, hVtxOutMult
-    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-branches, too-many-locals
     def process_histomass_single(self, index):
         myfile = TFile.Open(self.l_histomass[index], "recreate")
         dfevtorig = pickle.load(openfile(self.l_evtorig[index], "rb"))
+        neventsorig = len(dfevtorig)
         if self.s_trigger is not None:
             dfevtorig = dfevtorig.query(self.s_trigger)
+        neventsaftertrigger = len(dfevtorig)
         if self.runlistrigger is not None:
             dfevtorig = selectdfrunlist(dfevtorig, \
                              self.run_param[self.runlistrigger], "run_number")
+        neventsafterrunsel = len(dfevtorig)
         dfevtevtsel = dfevtorig.query(self.s_evtsel)
+
+        #validation plot for event selection
+        neventsafterevtsel = len(dfevtevtsel)
+        histonorm = TH1F("histonorm", "histonorm", 10, 0, 10)
+        histonorm.SetBinContent(1, neventsorig)
+        histonorm.GetXaxis().SetBinLabel(1, "tot events")
+        histonorm.SetBinContent(2, neventsaftertrigger)
+        histonorm.GetXaxis().SetBinLabel(2, "tot events after trigger")
+        histonorm.SetBinContent(3, neventsafterrunsel)
+        histonorm.GetXaxis().SetBinLabel(3, "tot events after run sel")
+        histonorm.SetBinContent(4, neventsafterevtsel)
+        histonorm.GetXaxis().SetBinLabel(4, "tot events after evt sel")
+        for ibin2 in range(len(self.lvar2_binmin)):
+            binneddf = seldf_singlevar_inclusive(dfevtevtsel, self.v_var2_binning_gen, \
+                self.lvar2_binmin[ibin2], self.lvar2_binmax[ibin2])
+            histonorm.SetBinContent(5 + ibin2, len(binneddf))
+            histonorm.GetXaxis().SetBinLabel(5 + ibin2, \
+                        "tot events after mult sel %d - %d" % \
+                        (self.lvar2_binmin[ibin2], self.lvar2_binmax[ibin2]))
+        histonorm.Write()
         labeltrigger = "hbit%svs%s" % (self.triggerbit, self.v_var2_binning_gen)
 
         myfile.cd()
@@ -203,7 +227,8 @@ class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-att
 
         if self.performtriggerturn is True:
             df_recodtrig = pd.concat(list_df_recodtrig)
-            #df_recodtrig = df_recodtrig.query("inv_mass>1.7 and inv_mass<2.05")
+            df_recodtrig = df_recodtrig.query("inv_mass>%f and inv_mass<%f" % \
+                                              (self.mass - 0.15, self.mass + 0.15))
             dfevtwithd = pd.merge(dfevtevtsel, df_recodtrig, on=self.v_evtmatch)
             label = "h%s" % self.v_var2_binning_gen
             histomult = TH1F(label, label, self.nbinshisto,
@@ -215,7 +240,9 @@ class ProcesserDhadrons_mult(Processer): # pylint: disable=too-many-instance-att
                                   self.minvaluehisto, self.maxvaluehisto)
             fill_hist(histomultwithd, dfevtwithd["%s_x" % self.v_var2_binning_gen])
             histomultwithd.Write()
-
+            valhistolist = fillvalidationvsmult(dfevtorig, dfevtevtsel, df_recodtrig)
+            for histo in valhistolist:
+                histo.Write()
     def process_histomass(self):
         print("Doing masshisto", self.mcordata, self.period)
         print("Using run selection for mass histo", \
---
+++ machine_learning_hep/utilities_validation.py
@@ -0,0 +1,57 @@
+#############################################################################
+##  © Copyright CERN 2018. All rights not expressly granted are reserved.  ##
+##                 Author: Gian.Michele.Innocenti@cern.ch                  ##
+## This program is free software: you can redistribute it and/or modify it ##
+##  under the terms of the GNU General Public License as published by the  ##
+## Free Software Foundation, either version 3 of the License, or (at your  ##
+## option) any later version. This program is distributed in the hope that ##
+##  it will be useful, but WITHOUT ANY WARRANTY; without even the implied  ##
+##     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    ##
+##           See the GNU General Public License for more details.          ##
+##    You should have received a copy of the GNU General Public License    ##
+##   along with this program. if not, see <https://www.gnu.org/licenses/>. ##
+#############################################################################
+
+"""
+Script containing all helper functions related to plotting with ROOT
+
+Script also contains the "class Errors", used for systematic uncertainties (to
+replace AliHFSystErr from AliPhysics).
+"""
+# pylint: disable=too-many-lines
+# pylint: disable=import-error, no-name-in-module
+from root_numpy import fill_hist # pylint: disable=import-error, no-name-in-module
+from ROOT import TH2F, TH1F
+from machine_learning_hep.utilities_plot import fill2dhist
+from machine_learning_hep.bitwise import filter_bit_df
+
+def fillvalidationvsmult(dfevt, dfevtevtsel, df_reco):
+    """
+    Create a TH3F histogram and fill it with three variables from a dataframe.
+    """
+    _ = len(df_reco)
+    hntrklvstrklcorr = TH2F("hntrklvstrklcorr", " ; n_tracklets ; n_tracklets_corr",
+                            200, -0.5, 199.5, 200, -0.5, 199.5)
+    fill2dhist(dfevtevtsel, hntrklvstrklcorr, "n_tracklets", "n_tracklets_corr")
+    hntrklcorrvszvtx = TH2F("hntrklcorrvszvtx", " ; z_vtx ; n_tracklets_corr",
+                            100, -15., 15, 200, -0.5, 199.5)
+    fill2dhist(dfevtevtsel, hntrklcorrvszvtx, "z_vtx_reco", "n_tracklets_corr")
+    hntrklvszvtx = TH2F("hntrklvszvtx", " ; z_vtx ; n_tracklets",
+                        100, -15., 15, 200, -0.5, 199.5)
+    fill2dhist(dfevtevtsel, hntrklvszvtx, "z_vtx_reco", "n_tracklets")
+
+    hntrklcorrsel = TH1F("hntrklcorrsel", ' ; ntracklets_corr ; Entries',
+                         200, -0.5, 199.5)
+    hntrklcorrselevtspd = TH1F("hntrklcorrselevtspd", ' ; ntracklets_corr ; Entries',
+                               200, -0.5, 199.5)
+    hntrklcorrpileup = TH1F("hntrklcorrpileup", ' ; ntracklets_corr ; Entries',
+                            200, -0.5, 199.5)
+    df_pileup = filter_bit_df(dfevt, 'is_ev_rej', [[4], []])
+    df_selevtspd = dfevtevtsel.query("is_ev_rej == 0 and \
+        trigger_hasclass_HighMultSPD == 1 and trigger_hasbit_HighMultSPD == 1")
+
+    fill_hist(hntrklcorrsel, dfevtevtsel["n_tracklets_corr"])
+    fill_hist(hntrklcorrselevtspd, df_selevtspd["n_tracklets_corr"])
+    fill_hist(hntrklcorrpileup, df_pileup["n_tracklets_corr"])
+    return [hntrklvstrklcorr, hntrklcorrvszvtx, hntrklvszvtx, hntrklcorrsel,
+            hntrklcorrpileup, hntrklcorrselevtspd]
