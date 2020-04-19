20efee24773b62dc68d7319c592a7e04da7150af
Nicolò Jacazio <njacazio@users.noreply.github.com>
2020-04-19 12:40:23 +0200

    Add validation of candidates (#630)
    * Rename validation functions
    
    * Rename validation script
    
    * Add validation script for candidates
    
    - Fix missing title
    
    * Implement validation histogram plotting
    
    * Remove unused input
    
    * Fix description
    
    * Rename flag performtriggerturn to event_cand_validation
    
    * Filling histograms for vertex, candidates and for MC
    
    * Add common validation file
    
    - Add vertex validation
    - Extend validation plots
    
    * Add plot of candidate validation
    
    - add MC info as well
    
    * Move save_root_object to root.py
    
    * Format code
    
    * Create dedicated directory for validation
    
    * Fix for style
    
    * Add extension to validation plots
    
    * Update Mult. Validation
    
    * Update validation of candidates
    
    * Saving all validation histograms
    
    - Add TPC-TOF M.E.
    - Add fraction of pileup events
    
    * Format validation
    
    * Add binning for diff in ntrklt
    
    * Format analyzerdhadrons_mult.py
    
    * Add check on verbosity
    
    * Remove pylint rules
    
    * Rename reset function
    
    * Remove split

---
+++ machine_learning_hep/validation/validation_vertex.py
@@ -0,0 +1,40 @@
+#############################################################################
+##  © Copyright CERN 2018. All rights not expressly granted are reserved.  ##
+##                 Author: Gian.Michele.Innocenti@cern.ch                  ##
+## This program is free software: you can redistribute it and/or modify it ##
+##  under the terms of the GNU General Public License as published by the  ##
+## Free Software Foundation, either version 3 of the License, or (at your  ##
+## option) any later version. This program is distributed in the hope that ##
+##  it will be useful, but WITHOUT ANY WARRANTY; without even the implied  ##
+##     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    ##
+##           See the GNU General Public License for more details.          ##
+##    You should have received a copy of the GNU General Public License    ##
+##   along with this program. if not, see <https://www.gnu.org/licenses/>. ##
+#############################################################################
+
+"""
+Script containing validation histograms on the event granularity for the vertex monitoring
+"""
+
+from machine_learning_hep.utilities_plot import buildbinning
+from machine_learning_hep.validation.validation import ValidationCollection
+
+
+def fill_validation_vertex(dfevt, dfevtevtsel, df_reco):
+    """
+    Create histograms for the validation on the event level as a function of the multiplicity
+    """
+    _ = len(df_reco)
+    __ = len(dfevtevtsel)
+
+    # Binning definition
+    # binning_xyvtx = buildbinning(100, -1.0, 1)
+    binning_zvtx = buildbinning(100, -15.0, 15)
+
+    # Make and fill histograms
+    val = ValidationCollection(dfevt[dfevt.is_ev_rej_INT7 == 0])
+    # val.make_and_fill(binning_xyvtx, "x_vtx_reco")
+    # val.make_and_fill(binning_xyvtx, "y_vtx_reco")
+    val.make_and_fill(binning_zvtx, "z_vtx_reco")
+
+    return val
