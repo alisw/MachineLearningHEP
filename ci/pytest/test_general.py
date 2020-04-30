54e81700e9a276b8e0bc71d91451f0d58f2b3d73
benedikt-voelkel <benedikt.voelkel@gmx.de>
2020-04-30 16:02:25 +0200

    Upgrade CI (#675)
    Set files to be ignored in ci/pylint-ignore
    
    Co-authored-by: Benedikt Volkel <benedikt.volkel@cern.ch>

--- ci/pytest/test_general.py
+++ ci/pytest/test_general.py
@@ -1,6 +1,6 @@
 from machine_learning_hep.io import parse_yaml
 
-YAML_PATH="ci/pytest/yaml.yaml"
+YAML_PATH = "ci/pytest/yaml.yaml"
 
 def test_yaml():
     assert isinstance(parse_yaml(YAML_PATH), dict)
