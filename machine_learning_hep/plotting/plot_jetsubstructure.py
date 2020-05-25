3ad7dd5d9aa4d834951e76c270f9015116425224
Vit Kucera <vit.kucera@cern.ch>
2020-05-25 04:58:05 +0200

    Add more MC comparison plots.

--- machine_learning_hep/plotting/plot_jetsubstructure.py
+++ machine_learning_hep/plotting/plot_jetsubstructure.py
@@ -23,7 +23,7 @@ import yaml
 from ROOT import TFile, TLatex, TLine, TGaxis, gROOT, gStyle
 from machine_learning_hep.utilities import make_message_notfound
 from machine_learning_hep.utilities import get_colour, get_marker, draw_latex
-from machine_learning_hep.utilities import make_plot
+from machine_learning_hep.utilities import make_plot, get_y_window_his, get_y_window_gr, get_plot_range
 from machine_learning_hep.logger import get_logger
 
 def main(): # pylint: disable=too-many-locals, too-many-statements, too-many-branches
@@ -461,6 +461,16 @@ def main(): # pylint: disable=too-many-locals, too-many-statements, too-many-bra
 
     # PYTHIA, HF, inclusive, quark, gluon
 
+    incl_pythia_syst_cl = incl_pythia_syst.Clone()
+
+    y_min_h, y_max_h = get_y_window_his([hf_pythia_stat, incl_pythia_stat, quark_pythia_stat, gluon_pythia_stat])
+    y_min_g, y_max_g = get_y_window_gr([incl_pythia_syst, quark_pythia_syst, gluon_pythia_syst])
+    y_min = min(y_min_h, y_min_g)
+    y_max = max(y_max_h, y_max_g)
+    y_margin_up = 0.46
+    y_margin_down = 0.05
+    y_min_plot, y_max_plot = get_plot_range(y_min, y_max, y_margin_down, y_margin_up)
+
     #leg_pos = [.6, .65, .75, .85]
     leg_pos = [.72, .55, .85, .85]
     list_obj = [incl_pythia_syst, quark_pythia_syst, gluon_pythia_syst, hf_pythia_stat, incl_pythia_stat, quark_pythia_stat, gluon_pythia_stat]
@@ -471,7 +481,7 @@ def main(): # pylint: disable=too-many-locals, too-many-statements, too-many-bra
     y_margin_down = 0.05
     cshape_mc, list_obj_mc_new = make_plot("cshape_mc_" + suffix, size=size_can, \
         list_obj=list_obj, labels_obj=labels_obj, opt_leg_g=opt_leg_g, opt_plot_g=opt_plot_g, offsets_xy=offsets_axes, \
-        colours=colours, markers=markers, leg_pos=leg_pos, margins_y=[y_margin_down, y_margin_up], margins_c=margins_can, \
+        colours=colours, markers=markers, leg_pos=leg_pos, range_y=[y_min_plot, y_max_plot], margins_c=margins_can, \
         title=title_full)
     cshape_ratio.Update()
     for gr, c in zip((incl_pythia_syst, quark_pythia_syst, gluon_pythia_syst), (c_incl_mc, c_quark_mc, c_gluon_mc)):
@@ -510,5 +520,108 @@ def main(): # pylint: disable=too-many-locals, too-many-statements, too-many-bra
         y_latex -= y_step
     cshape_mc.Update()
     cshape_mc.SaveAs("%s/%s_mc_%s.pdf" % (rootpath, shape, suffix))
+
+    # PYTHIA, HF, quark, gluon
+
+    #leg_pos = [.6, .65, .75, .85]
+    leg_pos = [.72, .61, .85, .85]
+    list_obj = [quark_pythia_syst, gluon_pythia_syst, hf_pythia_stat, quark_pythia_stat, gluon_pythia_stat]
+    labels_obj = ["quark", "gluon", "%s-tagged" % p_latexnhadron]
+    colours = [get_colour(i, j) for i, j in zip((c_quark_mc, c_gluon_mc, c_hf_mc, c_quark_mc, c_gluon_mc), (2, 2, 1, 1, 1))]
+    markers = [get_marker(i) for i in (c_quark_mc, c_gluon_mc, c_hf_mc, c_quark_mc, c_gluon_mc)]
+    y_margin_up = 0.46
+    y_margin_down = 0.05
+    cshape_mc, list_obj_mc_new = make_plot("cshape_mc_qgd_" + suffix, size=size_can, \
+        list_obj=list_obj, labels_obj=labels_obj, opt_leg_g=opt_leg_g, opt_plot_g=opt_plot_g, offsets_xy=offsets_axes, \
+        colours=colours, markers=markers, leg_pos=leg_pos, range_y=[y_min_plot, y_max_plot], margins_c=margins_can, \
+        title=title_full)
+    cshape_ratio.Update()
+    for gr, c in zip((quark_pythia_syst, gluon_pythia_syst), (c_quark_mc, c_gluon_mc)):
+        gr.SetMarkerColor(get_colour(c))
+    leg_mc = list_obj_mc_new[0]
+    leg_mc.SetTextSize(fontsize)
+    leg_mc.SetHeader(text_pythia_split)
+    if shape == "nsd":
+        quark_pythia_syst.GetXaxis().SetNdivisions(5)
+    cshape_mc.Update()
+    if shape == "rg":
+        # plot the theta_g axis
+        axis_rg = hf_pythia_stat.GetXaxis()
+        rg_min = axis_rg.GetBinLowEdge(axis_rg.GetFirst())
+        rg_max = axis_rg.GetBinUpEdge(axis_rg.GetLast())
+        thetag_min = rg_min / radius_jet
+        thetag_max = rg_max / radius_jet
+        y_axis = cshape_mc.GetUymax()
+        axis_thetag = TGaxis(rg_min, y_axis, rg_max, y_axis, thetag_min, thetag_max, 510, "-")
+        axis_thetag.SetTitle(title_thetag)
+        axis_thetag.SetTitleSize(size_thg)
+        axis_thetag.SetLabelSize(0.036)
+        axis_thetag.SetTitleFont(42)
+        axis_thetag.SetLabelFont(42)
+        axis_thetag.SetLabelOffset(0)
+        axis_thetag.SetTitleOffset(offset_thg)
+        cshape_mc.SetTickx(0)
+        axis_thetag.Draw("same")
+    # Draw LaTeX
+    y_latex = y_latex_top
+    list_latex_mc = []
+    for text_latex in [text_alice, text_jets, text_ptjet, text_pth, text_ptcut, text_sd]:
+        latex = TLatex(x_latex, y_latex, text_latex)
+        list_latex_mc.append(latex)
+        draw_latex(latex, textsize=fontsize)
+        y_latex -= y_step
+    cshape_mc.Update()
+    cshape_mc.SaveAs("%s/%s_mc_qgd_%s.pdf" % (rootpath, shape, suffix))
+
+    # PYTHIA, HF, inclusive
+
+    #leg_pos = [.6, .65, .75, .85]
+    leg_pos = [.72, .67, .85, .85]
+    list_obj = [incl_pythia_syst_cl, hf_pythia_stat, incl_pythia_stat]
+    labels_obj = ["inclusive", "%s-tagged" % p_latexnhadron]
+    colours = [get_colour(i, j) for i, j in zip((c_incl_mc, c_hf_mc, c_incl_mc), (2, 1, 1))]
+    markers = [get_marker(i) for i in (c_incl_mc, c_hf_mc, c_incl_mc)]
+    y_margin_up = 0.46
+    y_margin_down = 0.05
+    cshape_mc, list_obj_mc_new = make_plot("cshape_mc_id_" + suffix, size=size_can, \
+        list_obj=list_obj, labels_obj=labels_obj, opt_leg_g=opt_leg_g, opt_plot_g=opt_plot_g, offsets_xy=offsets_axes, \
+        colours=colours, markers=markers, leg_pos=leg_pos, range_y=[y_min_plot, y_max_plot], margins_c=margins_can, \
+        title=title_full)
+    cshape_ratio.Update()
+    incl_pythia_syst_cl.SetMarkerColor(get_colour(c_incl_mc))
+    leg_mc = list_obj_mc_new[0]
+    leg_mc.SetTextSize(fontsize)
+    leg_mc.SetHeader(text_pythia_split)
+    if shape == "nsd":
+        incl_pythia_syst_cl.GetXaxis().SetNdivisions(5)
+    cshape_mc.Update()
+    if shape == "rg":
+        # plot the theta_g axis
+        axis_rg = hf_pythia_stat.GetXaxis()
+        rg_min = axis_rg.GetBinLowEdge(axis_rg.GetFirst())
+        rg_max = axis_rg.GetBinUpEdge(axis_rg.GetLast())
+        thetag_min = rg_min / radius_jet
+        thetag_max = rg_max / radius_jet
+        y_axis = cshape_mc.GetUymax()
+        axis_thetag = TGaxis(rg_min, y_axis, rg_max, y_axis, thetag_min, thetag_max, 510, "-")
+        axis_thetag.SetTitle(title_thetag)
+        axis_thetag.SetTitleSize(size_thg)
+        axis_thetag.SetLabelSize(0.036)
+        axis_thetag.SetTitleFont(42)
+        axis_thetag.SetLabelFont(42)
+        axis_thetag.SetLabelOffset(0)
+        axis_thetag.SetTitleOffset(offset_thg)
+        cshape_mc.SetTickx(0)
+        axis_thetag.Draw("same")
+    # Draw LaTeX
+    y_latex = y_latex_top
+    list_latex_mc = []
+    for text_latex in [text_alice, text_jets, text_ptjet, text_pth, text_ptcut, text_sd]:
+        latex = TLatex(x_latex, y_latex, text_latex)
+        list_latex_mc.append(latex)
+        draw_latex(latex, textsize=fontsize)
+        y_latex -= y_step
+    cshape_mc.Update()
+    cshape_mc.SaveAs("%s/%s_mc_id_%s.pdf" % (rootpath, shape, suffix))
 
 main()
