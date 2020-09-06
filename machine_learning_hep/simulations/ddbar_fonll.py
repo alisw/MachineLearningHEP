581e28611c319fd3fd0ff4aac1bc41d32b026eb9
Gian Michele Innocenti <ginnocen@cern.ch>
2020-09-06 18:08:49 +0200

    update

--- machine_learning_hep/simulations/ddbar_fonll.py
+++ machine_learning_hep/simulations/ddbar_fonll.py
@@ -26,10 +26,10 @@ p_fonllband = 'max'
 ptmin = 0
 ptmax = 30
 delta_pt = ptmax - ptmin
-p_fragf = 0.6086
+p_fragf = 0.542
 f_fonll = "fo_pp_d0meson_5TeV_y0p5.csv"
 p_br = 3.95e-2
-p_ncoll = 401
+p_ncoll = 392
 p_sigmamb = 57.8e-3
 acc = 1
 p_fprompt = 1
@@ -37,9 +37,9 @@ p_fprompt = 1
 gROOT.SetBatch(True)
 
 pt_range = [0, 2, 4, 6, 8, 12, 16, 20, 30]
-eff_range = [0.1, 0.2, 0.3, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5]
-effAA_range = [0.001, 0.003, 0.01, 0.02, 0.06, 0.1, 0.1, 0.1, 0.1]
-raa_range = [0.8, 0.7, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3]
+eff_range = [0.01, 0.03, 0.07, 0.1, 0.15, 0.2, 0.25, 0.3]
+effAA_range = [0.001, 0.01, 0.04, 0.06, 0.1, 0.17, 0.18, 0.18]
+raa_range = [0.8, 0.7, 0.3, 0.2, 0.2, 0.2, 0.22, 0.3]
 bins = array('f', pt_range)
 
 hfonllc = TH1F("hfonllc", "", len(pt_range) - 1, bins)
@@ -63,22 +63,37 @@ for i, ptmin in enumerate(pt_range):
     binwidth = pt_range[i+1] - pt_range[i]
     df_fonll_in_pt = df_fonll.query('(pt >= @ptmin) and (pt < @ptmax)')[p_fonllband]
     crossc = df_fonll_in_pt.sum() * 1e-12 /binwidth
-    cross = df_fonll_in_pt.sum() * p_br * p_fragf * 1e-12 /binwidth
-    hfonllc.SetBinContent(i+1, crossc)
-    hfonllDtoKpi.SetBinContent(i+1, cross)
-    hyieldDtoKpirsel.SetBinContent(i+1, cross * binwidth * eff_range[i] / p_sigmamb)
-    hyieldDtoKpi.SetBinContent(i+1, cross * binwidth / p_sigmamb)
-    hyieldc.SetBinContent(i+1, crossc * binwidth / p_sigmamb)
-    hyieldDtoKpiAA.SetBinContent(i+1, cross * binwidth * p_ncoll / p_sigmamb)
-    hyieldDtoKpirselAA.SetBinContent(i+1, cross * binwidth * p_ncoll * effAA_range[i]/ p_sigmamb)
-    hyieldcAA.SetBinContent(i+1, crossc * binwidth * p_ncoll/ p_sigmamb)
+    yieldc = crossc * binwidth / p_sigmamb
+    crossDtoKpi = crossc * p_br * p_fragf
+    yieldDtoKpi = crossc * p_br * p_fragf * binwidth / p_sigmamb
+    yieldDtoKpirsel = crossc * p_br * p_fragf * binwidth * eff_range[i] / p_sigmamb
+    yieldcAA = crossc * binwidth * p_ncoll/ p_sigmamb
+    yieldDtoKpiAA = crossc * p_br * p_fragf * binwidth * p_ncoll * raa_range[i] / p_sigmamb
+    yieldDtoKpirselAA = crossc * p_br * p_fragf * binwidth * p_ncoll * raa_range[i] \
+                        * effAA_range[i] / p_sigmamb
 
-    hyieldDtoKpipairrsel.SetBinContent(i+1, cross * binwidth * eff_range[i] *
-                                       eff_range[i] * p_br * p_fragf/ p_sigmamb)
-    hyieldDtoKpipairrselAA.SetBinContent(i+1, cross * binwidth * effAA_range[i]
-                                         * effAA_range[i] * raa_range[i] *
-                                         raa_range[i] * p_br * p_fragf * p_ncoll/ p_sigmamb)
-    print("min,max", ptmin, ptmax, cross)
+    yieldDtoKpipairrsel = crossc * p_br * p_fragf * binwidth * eff_range[i]/ p_sigmamb \
+                          * p_br * p_fragf * eff_range[i]
+    yieldDtoKpipairrselAA = crossc * p_br * p_fragf * binwidth * p_ncoll \
+                            * raa_range[i] * effAA_range[i] / p_sigmamb \
+                            * p_br * p_fragf * raa_range[i] * effAA_range[i]
+
+
+
+    hfonllc.SetBinContent(i+1, crossc)
+    hyieldc.SetBinContent(i+1, yieldc)
+    hfonllDtoKpi.SetBinContent(i+1, crossDtoKpi)
+    hyieldDtoKpi.SetBinContent(i+1, yieldDtoKpi)
+    hyieldDtoKpirsel.SetBinContent(i+1, yieldDtoKpirsel)
+
+    hyieldcAA.SetBinContent(i+1, yieldcAA)
+    hyieldDtoKpiAA.SetBinContent(i+1, yieldDtoKpiAA)
+    hyieldDtoKpirselAA.SetBinContent(i+1, yieldDtoKpirselAA)
+
+
+    hyieldDtoKpipairrsel.SetBinContent(i+1, yieldDtoKpipairrsel)
+    hyieldDtoKpipairrselAA.SetBinContent(i+1, yieldDtoKpipairrselAA)
+    print("min,max", ptmin, ptmax, crossDtoKpi)
 
 load_root_style()
 
@@ -118,7 +133,7 @@ for i, _ in enumerate(xaxis_list):
     list_latex.append(latex)
     histo_list[i].Draw()
     draw_latex(list_latex[i], 1, 0.04)
-c.SaveAs("generated_level.pdf")
+c.SaveAs("charmstudies_perevent.pdf")
 
 #### estimated predictions ####
 hyieldDtoKpipairrsel2B = hyieldDtoKpipairrsel.Clone("hyieldDtoKpipairrsel2B")
@@ -138,8 +153,8 @@ yaxis_list_est = ["Counts", "Counts", "Counts", "Counts", "Counts"]
 text_list_est = ["D^{0}-D^{0}bar pairs pp recosel 2B",
                  "D^{0}-D^{0}bar pairs pp recosel 200B",
                  "D^{0}-D^{0}bar pairs pp recosel 100M",
-                 "D^{0}-D^{0}bar pairs pp recosel 50B",
-                 "D^{0}-D^{0}bar pairs pp recosel 2500B"]
+                 "D^{0}-D^{0}bar pairs AA recosel 50B",
+                 "D^{0}-D^{0}bar pairs AA recosel 2500B"]
 nevents_list_ext = [2e9, 200*2e9, 100*1e6, 50*1e9, 2500*1e9]
 
 for ihisto, _ in enumerate(histo_list_est):
@@ -160,4 +175,4 @@ for i, _ in enumerate(xaxis_list_est):
     list_est_latex.append(latex)
     histo_list_est[i].Draw("HIST")
     draw_latex(list_est_latex[i], 1, 0.04)
-c_est.SaveAs("generated_level_est.pdf")
+c_est.SaveAs("charmstudies_stats.pdf")
