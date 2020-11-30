5e8426dfe4232b7e7f047d767fb812402a2e7492
Gian Michele Innocenti <ginnocen@cern.ch>
2020-11-30 23:45:32 +0100

    add scenario2 database

--- machine_learning_hep/run5.sh
+++ machine_learning_hep/run5.sh
@@ -1,1 +1,2 @@
-DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero.yml -a scenario2
+#DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero_scenario3.yml -a scenario3
+DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero_scenario2.yml -a scenario2
