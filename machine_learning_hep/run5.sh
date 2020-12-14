9a76b2dbf1b03f9258a87183d72ee42a7a659f1e
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-12-14 08:50:49 +0100

    update script

--- machine_learning_hep/run5.sh
+++ machine_learning_hep/run5.sh
@@ -1,12 +1,20 @@
-rm -rf  /data/Run5data_mlhep/prod_test/pklmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pkldata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/evttotmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskdata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/evttotdata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmlmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmldata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/mltotmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/mltotdata_scenario3
-rm -rf mlout_scenario3* mlplot_scenario3*
-DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero_scenario3.yml -a scenario3
+#!/bin/bash
+
+reset
+
+Tag="scenario3"
+Remove="No"
+if [[ -z $Remove ]]; then
+    rm -rf /data/Run5data_mlhep/prod_test/pklmc_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/pkldata_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/pklskmc_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/evttotmc_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/pklskdata_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/evttotdata_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/pklskmlmc_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/pklskmldata_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/mltotmc_$Tag
+    rm -rf /data/Run5data_mlhep/prod_test/mltotdata_$Tag
+fi
+
+DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero_$Tag.yml -a $Tag |& tee run5.log
