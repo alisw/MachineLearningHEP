520dfbef3212b1076c45cd6d31bf81a3e88008c5
Nicolò Jacazio <njacazio@users.noreply.github.com>
2021-01-21 21:07:11 +0100

    Run5branch (#769)
    * Read invmass and y values from database + configurable acc selection
    
    * Use mass variable name from database
    
    * Fix pylint
    
    * Add FONLL prediction for pp 14 TeV Run5
    
    * Change to MB for data
    
    * Add yml for Scenario 2 (MB data)
    
    * update script
    
    * fix path for MB
    
    * Fix typo (#767)
    
    * Read invmass and y values from database + configurable acc selection
    
    * Use mass variable name from database
    
    * Fix intentation
    
    * Align to remote
    
    * Add efficiency/cross section computation for Run5
    
    * Cleaning before running
    
    * Format and default no FD for Run5
    
    Co-authored-by: Gian Michele Innocenti <ginnocen@cern.ch>
    Co-authored-by: Gian Michele Innocenti <gminnocen@gmail.com>

--- machine_learning_hep/analysis/analyzerdhadrons.py
+++ machine_learning_hep/analysis/analyzerdhadrons.py
@@ -37,12 +37,15 @@ from machine_learning_hep.root import save_root_object
 from machine_learning_hep.utilities_plot import plot_histograms
 from machine_learning_hep.analysis.analyzer import Analyzer
 # pylint: disable=too-few-public-methods, too-many-instance-attributes, too-many-statements, fixme
-class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
+
+
+class AnalyzerDhadrons(Analyzer):  # pylint: disable=invalid-name
     species = "analyzer"
+
     def __init__(self, datap, case, typean, period):
         super().__init__(datap, case, typean, period)
         self.logger = get_logger()
-        #namefiles pkl
+        # namefiles pkl
         self.v_var_binning = datap["var_binning"]
         self.lpt_finbinmin = datap["analysis"][self.typean]["sel_an_binmin"]
         self.lpt_finbinmax = datap["analysis"][self.typean]["sel_an_binmax"]
@@ -52,18 +55,20 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         self.triggerbit = datap["analysis"][self.typean].get("triggerbit", "")
 
         self.d_resultsallpmc = datap["analysis"][typean]["mc"]["results"][period] \
-                if period is not None else datap["analysis"][typean]["mc"]["resultsallp"]
+            if period is not None else datap["analysis"][typean]["mc"]["resultsallp"]
         self.d_resultsallpdata = datap["analysis"][typean]["data"]["results"][period] \
-                if period is not None else datap["analysis"][typean]["data"]["resultsallp"]
+            if period is not None else datap["analysis"][typean]["data"]["resultsallp"]
 
         n_filemass_name = datap["files_names"]["histofilename"]
         self.n_filemass = os.path.join(self.d_resultsallpdata, n_filemass_name)
-        self.n_filemass_mc = os.path.join(self.d_resultsallpmc, n_filemass_name)
+        self.n_filemass_mc = os.path.join(
+            self.d_resultsallpmc, n_filemass_name)
         self.p_mass_fit_lim = datap["analysis"][self.typean]['mass_fit_lim']
 
         # Output directories and filenames
         self.yields_filename = "yields"
-        self.fits_dirname = os.path.join(self.d_resultsallpdata, f"fits_{case}_{typean}")
+        self.fits_dirname = os.path.join(
+            self.d_resultsallpdata, f"fits_{case}_{typean}")
         self.yields_syst_filename = "yields_syst"
         self.efficiency_filename = "efficiencies"
         self.sideband_subtracted_filename = "sideband_subtracted"
@@ -71,11 +76,12 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         self.n_fileff = datap["files_names"]["efffilename"]
         self.n_fileff = os.path.join(self.d_resultsallpmc, self.n_fileff)
         self.p_bin_width = datap["analysis"][self.typean]['bin_width']
-        self.p_num_bins = int(round((self.p_mass_fit_lim[1] - self.p_mass_fit_lim[0]) / \
+        self.p_num_bins = int(round((self.p_mass_fit_lim[1] - self.p_mass_fit_lim[0]) /
                                     self.p_bin_width))
-        #parameter fitter
+        # parameter fitter
         self.sig_fmap = {"kGaus": 0, "k2Gaus": 1, "kGausSigmaRatioPar": 2}
-        self.bkg_fmap = {"kExpo": 0, "kLin": 1, "Pol2": 2, "kNoBk": 3, "kPow": 4, "kPowEx": 5}
+        self.bkg_fmap = {"kExpo": 0, "kLin": 1,
+                         "Pol2": 2, "kNoBk": 3, "kPow": 4, "kPowEx": 5}
         # For initial fit in integrated mult bin
         self.init_fits_from = datap["analysis"][self.typean]["init_fits_from"]
         self.p_sgnfunc = datap["analysis"][self.typean]["sgnfunc"]
@@ -85,11 +91,16 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         self.p_massmax = datap["analysis"][self.typean]["massmax"]
         self.rebins = datap["analysis"][self.typean]["rebin"]
 
-        self.p_includesecpeaks = datap["analysis"][self.typean].get("includesecpeak", None)
-        self.p_masssecpeak = datap["analysis"][self.typean].get("masssecpeak", None)
-        self.p_fix_masssecpeaks = datap["analysis"][self.typean].get("fix_masssecpeak", None)
-        self.p_widthsecpeak = datap["analysis"][self.typean].get("widthsecpeak", None)
-        self.p_fix_widthsecpeak = datap["analysis"][self.typean].get("fix_widthsecpeak", None)
+        self.p_includesecpeaks = datap["analysis"][self.typean].get(
+            "includesecpeak", None)
+        self.p_masssecpeak = datap["analysis"][self.typean].get(
+            "masssecpeak", None)
+        self.p_fix_masssecpeaks = datap["analysis"][self.typean].get(
+            "fix_masssecpeak", None)
+        self.p_widthsecpeak = datap["analysis"][self.typean].get(
+            "widthsecpeak", None)
+        self.p_fix_widthsecpeak = datap["analysis"][self.typean].get(
+            "fix_widthsecpeak", None)
         if self.p_includesecpeaks is None:
             self.p_includesecpeaks = [False for ipt in range(self.p_nptbins)]
             self.p_masssecpeak = None
@@ -114,29 +125,47 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         self.ptranges = self.lpt_finbinmin.copy()
         self.ptranges.append(self.lpt_finbinmax[-1])
         # More specific fit options
-        self.include_reflection = datap["analysis"][self.typean].get("include_reflection", False)
+        self.include_reflection = datap["analysis"][self.typean].get(
+            "include_reflection", False)
 
         self.p_nevents = datap["analysis"][self.typean]["nevents"]
         self.p_sigmamb = datap["ml"]["opt"]["sigma_MB"]
         self.p_br = datap["ml"]["opt"]["BR"]
 
         # Systematics
-        self.mt_syst_dict = datap["analysis"][self.typean].get("systematics", None)
-        self.d_mt_results_path = os.path.join(self.d_resultsallpdata, "multi_trial")
+        self.mt_syst_dict = datap["analysis"][self.typean].get(
+            "systematics", None)
+        self.d_mt_results_path = os.path.join(
+            self.d_resultsallpdata, "multi_trial")
 
         self.p_indexhpt = datap["analysis"]["indexhptspectrum"]
         self.p_fd_method = datap["analysis"]["fd_method"]
         self.p_cctype = datap["analysis"]["cctype"]
         self.p_sigmav0 = datap["analysis"]["sigmav0"]
         self.p_inputfonllpred = datap["analysis"]["inputfonllpred"]
-        self.p_triggereff = datap["analysis"][self.typean].get("triggereff", [1])
-        self.p_triggereffunc = datap["analysis"][self.typean].get("triggereffunc", [0])
+        self.p_triggereff = datap["analysis"][self.typean].get("triggereff", [
+                                                               1])
+        self.p_triggereffunc = datap["analysis"][self.typean].get(
+            "triggereffunc", [0])
 
         self.root_objects = []
 
         # Fitting
         self.fitter = None
-        self.p_performval = datap["analysis"].get("event_cand_validation", None)
+        self.p_performval = datap["analysis"].get(
+            "event_cand_validation", None)
+
+        # HFPtSpectrum
+        self.p_year = 7  # "k2018"
+        self.p_energy = 1  # "k5dot023"
+        self.p_raavsep = 0  # "kPhiIntegrated"
+        self.p_epresolfile = ""
+        self.p_pbpbeloss = False
+        self.p_rapslice = 0  # "kdefault"
+        self.p_partantipart = True
+        self.p_analysis = 0  # "kTopological"
+        self.p_ccestimator = 0  # "kV0M"
+        self.p_useptdepeffunc = True
 
     # pylint: disable=import-outside-toplevel
     def fit(self):
@@ -157,7 +186,6 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         # Reset to former mode
         gROOT.SetBatch(tmp_is_root_batch)
 
-
     def yield_syst(self):
         # Enable ROOT batch mode and reset in the end
         tmp_is_root_batch = gROOT.IsBatch()
@@ -166,7 +194,8 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
             self.fitter = MLFitter(self.case, self.datap, self.typean,
                                    self.n_filemass, self.n_filemass_mc)
             if not self.fitter.load_fits(self.fits_dirname):
-                self.logger.error("Cannot load fits from dir %s", self.fits_dirname)
+                self.logger.error(
+                    "Cannot load fits from dir %s", self.fits_dirname)
                 return
 
         # Additional directory needed where the intermediate results of the multi trial are
@@ -179,11 +208,70 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
         # Reset to former mode
         gROOT.SetBatch(tmp_is_root_batch)
 
-    #def efficiency(self):
-    #To be added from dhadron_mult
+    def efficiency(self):
+        self.loadstyle()
+        print(self.n_fileff)
+        lfileeff = TFile.Open(self.n_fileff)
+        lfileeff.ls()
+        fileouteff = TFile.Open("%s/efficiencies%s%s.root" % (self.d_resultsallpmc,
+                                                              self.case, self.typean), "recreate")
+        cEff = TCanvas('cEff', 'The Fit Canvas')
+        cEff.SetCanvasSize(1900, 1500)
+        cEff.SetWindowSize(500, 500)
 
-    #def plotter(self):
-    #To be added from dhadron_mult
+        legeff = TLegend(.5, .65, .7, .85)
+        legeff.SetBorderSize(0)
+        legeff.SetFillColor(0)
+        legeff.SetFillStyle(0)
+        legeff.SetTextFont(42)
+        legeff.SetTextSize(0.035)
+
+        h_gen_pr = lfileeff.Get("h_gen_pr")
+        h_sel_pr = lfileeff.Get("h_sel_pr")
+        h_sel_pr.Divide(h_sel_pr, h_gen_pr, 1.0, 1.0, "B")
+        h_sel_pr.Draw("same")
+        fileouteff.cd()
+        h_sel_pr.SetName("eff")
+        h_sel_pr.Write()
+        h_sel_pr.GetXaxis().SetTitle("#it{p}_{T} (GeV/#it{c})")
+        h_sel_pr.GetYaxis().SetTitle("Acc x efficiency (prompt) %s %s (1/GeV)"
+                                     % (self.p_latexnhadron, self.typean))
+        h_sel_pr.SetMinimum(0.001)
+        h_sel_pr.SetMaximum(1.0)
+        gPad.SetLogy()
+        cEff.SaveAs("%s/Eff%s%s.eps" % (self.d_resultsallpmc,
+                                        self.case, self.typean))
+
+        cEffFD = TCanvas('cEffFD', 'The Fit Canvas')
+        cEffFD.SetCanvasSize(1900, 1500)
+        cEffFD.SetWindowSize(500, 500)
+
+        legeffFD = TLegend(.5, .65, .7, .85)
+        legeffFD.SetBorderSize(0)
+        legeffFD.SetFillColor(0)
+        legeffFD.SetFillStyle(0)
+        legeffFD.SetTextFont(42)
+        legeffFD.SetTextSize(0.035)
+
+        h_gen_fd = lfileeff.Get("h_gen_fd")
+        h_sel_fd = lfileeff.Get("h_sel_fd")
+        h_sel_fd.Divide(h_sel_fd, h_gen_fd, 1.0, 1.0, "B")
+        h_sel_fd.Draw("same")
+        fileouteff.cd()
+        h_sel_fd.SetName("eff_fd")
+        h_sel_fd.Write()
+        h_sel_fd.GetXaxis().SetTitle("#it{p}_{T} (GeV/#it{c})")
+        h_sel_fd.GetYaxis().SetTitle("Acc x efficiency feed-down %s %s (1/GeV)"
+                                     % (self.p_latexnhadron, self.typean))
+        h_sel_fd.SetMinimum(0.001)
+        h_sel_fd.SetMaximum(1.)
+        gPad.SetLogy()
+        legeffFD.Draw()
+        cEffFD.SaveAs("%s/EffFD%s%s.eps" % (self.d_resultsallpmc,
+                                            self.case, self.typean))
+
+    # def plotter(self):
+    # To be added from dhadron_mult
 
     @staticmethod
     def calculate_norm(hsel, hnovt, hvtxout):
@@ -205,17 +293,80 @@ class AnalyzerDhadrons(Analyzer): # pylint: disable=invalid-name
             norm = (n_sel + n_novtx) - n_novtx * n_vtxout / (n_sel + n_vtxout)
         return norm
 
-    def makenormyields(self): # pylint: disable=import-outside-toplevel, too-many-branches
+    def makenormyields(self):  # pylint: disable=import-outside-toplevel, too-many-branches
+        gROOT.SetBatch(True)
+        self.loadstyle()
+
+        yield_filename = self.make_file_path(self.d_resultsallpdata, self.yields_filename, "root",
+                                             None, [self.case, self.typean])
+        if not os.path.exists(yield_filename):
+            self.logger.fatal(
+                "Yield file %s could not be found", yield_filename)
+
+        fileouteff = f"{self.d_resultsallpmc}/efficiencies{self.case}{self.typean}.root"
+        if not os.path.exists(fileouteff):
+            self.logger.fatal(
+                "Efficiency file %s could not be found", fileouteff)
+
+        fileoutcross = "%s/finalcross%s%s.root" % \
+            (self.d_resultsallpdata, self.case, self.typean)
+
+        namehistoeffprompt = "eff"
+        namehistoefffeed = "eff_fd"
+        nameyield = "hyields0"
+
+        gROOT.LoadMacro("HFPtSpectrum.C")
+        from ROOT import HFPtSpectrum
+        histonorm = TH1F("histonorm", "histonorm", 1, 0, 1)
+
         filemass = TFile.Open(self.n_filemass)
         labeltrigger = "hbit%s" % (self.triggerbit)
         hsel = filemass.Get("sel_%s" % labeltrigger)
         hnovtx = filemass.Get("novtx_%s" % labeltrigger)
         hvtxout = filemass.Get("vtxout_%s" % labeltrigger)
         norm = self.calculate_norm(hsel, hnovtx, hvtxout)
+        histonorm.SetBinContent(1, norm)
         self.logger.warning("Number of events %d", norm)
+        print(self.p_inputfonllpred)
 
-    #def plotternormyields(self):
-    #To be added from dhadron_mult
+        HFPtSpectrum(self.p_indexhpt,
+                     self.p_inputfonllpred,
+                     fileouteff,
+                     namehistoeffprompt,
+                     namehistoefffeed,
+                     yield_filename,
+                     nameyield,
+                     fileoutcross,
+                     norm,
+                     self.p_sigmav0 * 1e12,
+                     self.p_fd_method,
+                     self.p_cctype,
+                     self.p_year,
+                     self.p_energy,
+                     self.p_raavsep,
+                     self.p_epresolfile,
+                     self.p_pbpbeloss,
+                     self.p_rapslice,
+                     self.p_partantipart,
+                     self.p_analysis,
+                     self.p_ccestimator,
+                     self.p_useptdepeffunc)
 
-    #def plottervalidation(self):
-    #To be added from dhadron_mult
+        fileoutcrosstot = TFile.Open("%s/finalcross%s%stot.root" %
+                                     (self.d_resultsallpdata, self.case, self.typean), "recreate")
+
+        fileoutcross = "%s/finalcross%s%s.root" % \
+            (self.d_resultsallpdata, self.case, self.typean)
+        f_fileoutcross = TFile.Open(fileoutcross)
+        if f_fileoutcross:
+            hcross = f_fileoutcross.Get("histoSigmaCorr")
+            fileoutcrosstot.cd()
+            hcross.Write()
+        histonorm.Write()
+        fileoutcrosstot.Close()
+
+    # def plotternormyields(self):
+    # To be added from dhadron_mult
+
+    # def plottervalidation(self):
+    # To be added from dhadron_mult
