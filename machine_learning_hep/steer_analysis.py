04366f82e75ab5d4be8080256afe242d70ab86b2
talazare <tatiana.lazareva@cern.ch>
2021-02-22 10:16:40 +0100

    do efficiency folded as separate step

--- machine_learning_hep/steer_analysis.py
+++ machine_learning_hep/steer_analysis.py
@@ -418,7 +418,7 @@ def do_entire_analysis(data_config: dict, data_param: dict, data_param_overwrite
     if dosyst is True:
         analyze_steps.append("yield_syst")
     if doeff is True:
-        analyze_steps.append("efficiency")
+        analyze_steps.append("efficiency_folded")
     if dojetstudies is True:
         if dofit is False:
             analyze_steps.append("fit")
