e97dd4f944dfa57647b2938fa1f6ecaf4a03cc4a
Gian Michele Innocenti <ginnocen@cern.ch>
2021-10-08 17:39:04 +0200

    Add database for X analysis

--- machine_learning_hep/run5.sh
+++ machine_learning_hep/run5.sh
@@ -1,12 +1,17 @@
-rm -rf  /data/Run5data_mlhep/prod_test/pklmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pkldata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/evttotmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskdata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/evttotdata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmlmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/pklskmldata_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/mltotmc_scenario3
-rm -rf  /data/Run5data_mlhep/prod_test/mltotdata_scenario3
-rm -rf mlout_scenario3* mlplot_scenario3*
-DISPLAY="" python do_entire_analysis.py -r submission/default_complete.yml -d data/data_run5/database_ml_parameters_Dzero_scenario3.yml -a scenario3
+rm -rf  /data/X3872ALICE3/extraone/mc/pkldata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/data/pkldata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/mc/pklskdata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/mc/evttotdata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/data/pklskdata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/data/evttotdata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/mc/pklskmldata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/data/pklskmldata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/mc/mltotdata_scenario3
+rm -rf  /data/X3872ALICE3/extraone/data/mltotdata_scenario3
+rm -rf  mlout
+rm -rf  mlplot
+
+
+DISPLAY="" python do_entire_analysis.py -r default_complete.yml -d data/data_run5/database_ml_parameters_Xanalysis_scenario3.yml -a scenario3
+
+
