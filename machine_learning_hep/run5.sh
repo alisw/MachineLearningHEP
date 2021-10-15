3c5fc62b45204240fdfa09d5e67025c84a88c0fd
Gian Michele Innocenti <ginnocen@cern.ch>
2021-10-15 13:57:47 +0200

    Fix X database

--- machine_learning_hep/run5.sh
+++ machine_learning_hep/run5.sh
@@ -1,16 +1,13 @@
-rm -rf  /data/X3872ALICE3/extraone/mc/pkldata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/data/pkldata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/mc/pklskdata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/mc/evttotdata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/data/pklskdata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/data/evttotdata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/mc/pklskmldata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/data/pklskmldata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/mc/mltotdata_scenario3
-rm -rf  /data/X3872ALICE3/extraone/data/mltotdata_scenario3
-rm -rf  mlout
-rm -rf  mlplot
-
+rm -rf  /data/X3872ALICE3/newvariables/mc/pkldata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/data/pkldata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/mc/pklskdata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/mc/evttotdata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/data/pklskdata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/data/evttotdata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/mc/pklskmldata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/data/pklskmldata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/mc/mltotdata_scenario3
+rm -rf  /data/X3872ALICE3/newvariables/data/mltotdata_scenario3
 
 DISPLAY="" python do_entire_analysis.py -r default_complete.yml -d data/data_run5/database_ml_parameters_Xanalysis_scenario3.yml -a scenario3
 
