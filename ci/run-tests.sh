ad2f20a0f98ae3674852552a45c586377aac4d08
Jochen Klein <jochen.klein@cern.ch>
2023-11-16 16:08:47 +0100

    Generalize copyright check

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -124,9 +124,9 @@ function test-pytest()
 
 
 function copyright-single() {
-  local COPYRIGHT="$(cat <<'EOF'
+  local COPYRIGHT_GIAN="$(cat <<'EOF'
 #############################################################################
-##  © Copyright CERN 2018. All rights not expressly granted are reserved.  ##
+##  © Copyright CERN 2023. All rights not expressly granted are reserved.  ##
 ##                 Author: Gian.Michele.Innocenti@cern.ch                  ##
 ## This program is free software: you can redistribute it and/or modify it ##
 ##  under the terms of the GNU General Public License as published by the  ##
@@ -140,8 +140,24 @@ function copyright-single() {
 #############################################################################
 EOF
 )"
+  local COPYRIGHT="$(cat <<'EOF'
+#############################################################################
+##  © Copyright CERN 2023. All rights not expressly granted are reserved.  ##
+##                                                                         ##
+## This program is free software: you can redistribute it and/or modify it ##
+##  under the terms of the GNU General Public License as published by the  ##
+## Free Software Foundation, either version 3 of the License, or (at your  ##
+## option) any later version. This program is distributed in the hope that ##
+##  it will be useful, but WITHOUT ANY WARRANTY; without even the implied  ##
+##     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    ##
+##           See the GNU General Public License for more details.          ##
+##    You should have received a copy of the GNU General Public License    ##
+##   along with this program. if not, see <https://www.gnu.org/licenses/>. ##
+#############################################################################
+EOF
+)"
   local COPYRIGHT_LINES=$(echo "$COPYRIGHT" | wc -l)
-  [[ "$(head -n$COPYRIGHT_LINES "$1")" == "$COPYRIGHT" ]] || { printf "$1: missing or malformed copyright notice\n"; return 1; }
+  [[ "$(head -n$COPYRIGHT_LINES "$1")" == "$COPYRIGHT_GIAN" ]] || [[ "$(head -n$COPYRIGHT_LINES "$1")" == "$COPYRIGHT" ]] || { printf "$1: missing or malformed copyright notice\n"; return 1; }
   return 0
 }
 
