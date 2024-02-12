2e12317607a21a55623e95ebef29c7d68b3ed107
Luigi Dello Stritto <ldellost@alicecerno2.cern.ch>
2024-02-12 15:30:33 +0100

    fix copyright

--- machine_learning_hep/hf_pt_spectrum.py
+++ machine_learning_hep/hf_pt_spectrum.py
@@ -1,6 +1,6 @@
 #############################################################################
 ##  © Copyright CERN 2023. All rights not expressly granted are reserved.  ##
-##                 Author: fabrizio.grosa@cern.ch                          ##
+##                 Author: Gian.Michele.Innocenti@cern.ch                  ##
 ## This program is free software: you can redistribute it and/or modify it ##
 ##  under the terms of the GNU General Public License as published by the  ##
 ## Free Software Foundation, either version 3 of the License, or (at your  ##
