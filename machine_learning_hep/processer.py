72d81d83268d9c6418b1f8026cc147f4578e74e6
Luigi Dello Stritto <ldellost@alicecerno2.cern.ch>
2024-07-05 16:18:59 +0200

    Save a component of a dataframe column array as a new column

--- machine_learning_hep/processer.py
+++ machine_learning_hep/processer.py
@@ -372,6 +372,11 @@ class Processer: # pylint: disable=too-many-instance-attributes
                     self.logger.debug(' %s -> extra', df_name)
                     for col_name, col_val in df_spec['extra'].items():
                         dfs[df_name][col_name] = dfs[df_name].eval(col_val)
+                if 'extract_component' in df_spec:
+                    self.logger.debug(' %s -> extract_component', df_name)
+                    specs = df_spec['extract_component']
+                    for spec in specs:
+                        dfs[df_name][spec['newvar']] = dfs[df_name][spec['var']].apply(lambda x: x[spec['component']])
                 if 'filter' in df_spec:
                     self.logger.debug(' %s -> filter', df_name)
                     dfquery(dfs[df_name], df_spec['filter'], inplace=True)
