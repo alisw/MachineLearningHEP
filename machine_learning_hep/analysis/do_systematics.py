640e66874dbbbc832cb498df42ba217b6c85fa87
Vít Kučera <vit.kucera@cern.ch>
2024-09-03 14:31:35 +0200

    Systematcs: Fix bin edges

--- machine_learning_hep/analysis/do_systematics.py
+++ machine_learning_hep/analysis/do_systematics.py
@@ -134,12 +134,13 @@ class AnalyzerJetSystematics:
         self.n_bins_obs_rec = len(binning_obs_rec) - 1
         self.obs_rec_min = float(binning_obs_rec[0])
         self.obs_rec_max = float(binning_obs_rec[-1])
+        self.edges_obs_rec = binning_obs_rec
         # binning_obs_rec = datap["analysis"][self.typean]["observables"][self.var]["bins_det_fix"]
         # self.n_bins_obs_rec = binning_obs_rec[0]
         # self.obs_rec_min = float(binning_obs_rec[1])
         # self.obs_rec_max = float(binning_obs_rec[2])
-        step = (self.obs_rec_max - self.obs_rec_min) / self.n_bins_obs_rec
-        self.edges_obs_rec = [round(self.obs_rec_min + i * step, 2) for i in range(self.n_bins_obs_rec + 1)]
+        # step = (self.obs_rec_max - self.obs_rec_min) / self.n_bins_obs_rec
+        # self.edges_obs_rec = [round(self.obs_rec_min + i * step, 2) for i in range(self.n_bins_obs_rec + 1)]
 
         # generator level
         if binning := self.cfg(f'observables.{var}.bins_gen_var'):
@@ -157,12 +158,13 @@ class AnalyzerJetSystematics:
         self.n_bins_obs_gen = len(binning_obs_gen) - 1
         self.obs_gen_min = float(binning_obs_gen[0])
         self.obs_gen_max = float(binning_obs_gen[-1])
+        self.edges_obs_gen = binning_obs_gen
         # binning_obs_gen = datap["analysis"][self.typean]["observables"][self.var]["bins_gen_fix"]
         # self.n_bins_obs_gen = binning_obs_gen[0]
         # self.obs_gen_min = float(binning_obs_gen[1])
         # self.obs_gen_max = float(binning_obs_gen[2])
-        step = (self.obs_gen_max - self.obs_gen_min) / self.n_bins_obs_gen
-        self.edges_obs_gen = [round(self.obs_gen_min + i * step, 2) for i in range(self.n_bins_obs_gen + 1)]
+        # step = (self.obs_gen_max - self.obs_gen_min) / self.n_bins_obs_gen
+        # self.edges_obs_gen = [round(self.obs_gen_min + i * step, 2) for i in range(self.n_bins_obs_gen + 1)]
 
         print("Rec obs edges:", self.edges_obs_rec, "Gen obs edges:", self.edges_obs_gen)
         print("Rec ptjet edges:", self.edges_ptjet_rec, "Gen ptjet edges:", self.edges_ptjet_gen)
