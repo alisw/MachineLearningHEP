bbaefc09d280622f42f65154933119a8c39155fd
Vít Kučera <vit.kucera@cern.ch>
2024-09-05 04:06:13 +0200

    Add plotting wrapper method. Plot efficiencies.

--- machine_learning_hep/plotting/plot_jetsubstructure_run3.py
+++ machine_learning_hep/plotting/plot_jetsubstructure_run3.py
@@ -27,7 +27,7 @@ from functools import reduce
 import numpy as np
 
 import yaml
-from ROOT import TFile, gROOT  # , gStyle
+from ROOT import TFile, gROOT, gStyle
 
 from machine_learning_hep.logger import get_logger
 from machine_learning_hep.analysis.analyzer_jets import string_range_ptjet, string_range_pthf
@@ -86,6 +86,7 @@ class Plotter:
         # If input file path is not provided, take the analysis result.
         self.path_input_file = path_input_file if path_input_file else self.path_file_results
         self.dir_input = os.path.dirname(self.path_input_file)
+        self.file_results = None
 
         print(f"Input file: {self.path_input_file}")
 
@@ -98,9 +99,17 @@ class Plotter:
         print(f"Plots will be saved in {self.dir_out_figs}")
 
         # plotting
+        self.list_obj = []
+        self.labels_obj = []
+        self.list_latex = []
+        self.list_colours = []
+        self.list_markers = []
+        self.list_new = []  # list to avoid loosing objects created in loops
+
         # LaTeX string
         self.latex_hadron = self.db_typean["latexnamehadron"]
         self.latex_ptjet = "#it{p}_{T}^{jet ch}"
+        self.latex_pthf = "#it{p}_{T}^{%s} (GeV/#it{c})" % self.latex_hadron
         self.latex_obs = self.db_typean["observables"][self.var]["label"]
         self.latex_y = self.db_typean["observables"][self.var]["label_y"]
 
@@ -109,7 +118,6 @@ class Plotter:
         self.n_bins_pthf = len(self.edges_pthf) - 1
 
         # binning of jet pt
-        self.ptjet_name = "ptjet"  # name
         # reconstruction level
         self.edges_ptjet_rec = self.db_typean["bins_ptjet"]
         self.n_bins_ptjet_rec = len(self.edges_ptjet_rec) - 1
@@ -168,14 +176,15 @@ class Plotter:
         # self.size_can = [800, 800]
         self.offsets_axes = [0.8, 1.1]
         self.margins_can = [0.1, 0.13, 0.05, 0.03]
-        # self.fontsize = 0.035
+        self.fontsize = 0.035
         self.opt_leg_g = "FP"  # for systematic uncertainties in the legend
         self.opt_plot_g = "2"
-        # self.x_latex = 0.18
-        # self.y_latex_top = 0.88
+        self.x_latex = 0.18
+        self.y_latex_top = 0.88
         self.y_step = 0.05
         self.leg_pos = [.72, .75, .85, .85]
-        self.y_margin_up = 0.46
+        # self.y_margin_up = 0.46
+        self.y_margin_up = 0.05
         self.y_margin_down = 0.05
         # size_can_double = [800, 800]
         # offsets_axes_double = [0.8, 0.8]
@@ -186,11 +195,13 @@ class Plotter:
         # fontsize_glob = 0.032 # font size relative to the canvas height
         # scale_title = 1.3 # scaling factor to increase the size of axis titles
         # tick_length = 0.02
+        self.plot_errors_x = True  # plot horizontal error bars
 
         # axes titles
         self.title_x = self.latex_obs
         self.title_y = self.latex_y
-        self.title_full = ";%s;%s" % (self.title_x, self.title_y)
+        self.title_full = f";{self.title_x};{self.title_y}"
+        self.title_full_default = self.title_full
         # self.title_full_ratio = ";%s;data/MC: ratio of %s" % (self.title_x, self.title_y)
         # text
         self.text_alice = "ALICE Preliminary, pp, #sqrt{#it{s}} = 13.6 TeV"  # preliminaries
@@ -229,10 +240,13 @@ class Plotter:
         graph.GetXaxis().SetLimits(round(x_range[var][0], 2), round(x_range[var][1], 2))
         reset_graph_outside_range(graph, *x_range[var])
 
-    def get_object(self, name: str, file):
-        if not (obj := file.Get(name)):
+    def get_object(self, name: str):
+        if not (obj := self.file_results.Get(name)):
             self.logger.fatal(make_message_notfound(name))
         return obj
+
+    def get_objects(self, *names: str):
+        return [self.get_object(name) for name in names]
 
     def report_means(self, h_stat, h_syst, iptjet):
         mean_z_stat = get_mean_hist(h_stat)
@@ -266,17 +280,57 @@ class Plotter:
         pt_max = min(pt_max, self.edges_ptjet_gen[iptjet + 1]) if iptjet > -1 else pt_max
         return self.text_pth % (pt_min, self.latex_hadron, pt_max, self.latex_hadron)
 
+    def make_plot(self, name: str):
+        """Wrapper method for calling make_plot and saving the canvas."""
+        assert all(self.list_obj)
+        n_obj = len(self.list_obj)
+        assert len(self.labels_obj) == n_obj
+        self.list_colours = [get_colour(i, 1) for i in range(n_obj)]
+        self.list_markers = [get_marker(i) for i in range(n_obj)]
+        y_margin_up_adj = self.y_margin_up + 1. - self.y_latex_top + self.y_step * (len(self.list_latex) - 1)
+        can, new = make_plot(name,
+                             list_obj=self.list_obj, labels_obj=self.labels_obj,
+                             opt_leg_g=self.opt_leg_g, opt_plot_g=self.opt_plot_g,
+                             offsets_xy=self.offsets_axes,
+                             colours=self.list_colours, markers=self.list_markers, leg_pos=self.leg_pos,
+                             margins_y=[self.y_margin_down, y_margin_up_adj], margins_c=self.margins_can,
+                             title=self.title_full)
+        self.list_new += new
+        if self.list_latex:
+            self.list_new += draw_latex_lines(self.list_latex,
+                                              x_start=self.x_latex, y_start=self.y_latex_top,
+                                              y_step=self.y_step, font_size=self.fontsize)
+        gStyle.SetErrorX(0.5)  # reset default width
+        if not self.plot_errors_x:
+            gStyle.SetErrorX(0)  # do not plot horizontal error bars of histograms
+        self.save_canvas(can)
+        gStyle.SetErrorX(0.5)  # reset default width
+        return can, new
+
     def plot(self):
         print("Observable:", self.var)
 
-        # gStyle.SetErrorX(0) # do not plot horizontal error bars of histograms
-
-        list_new = []  # list to avoid loosing objects created in loops
-
-        with TFile.Open(self.path_input_file) as file_results:
+        with TFile.Open(self.path_input_file) as self.file_results:
             name_hist_unfold_2d = f"h_ptjet-{self.var}_{self.method}_unfolded_data_0"
-            hist_unfold = self.get_object(name_hist_unfold_2d, file_results)
+            hist_unfold = self.get_object(name_hist_unfold_2d)
             axis_ptjet = get_axis(hist_unfold, 0)
+
+            # Efficiency
+            print("Plotting efficiency")
+            self.list_obj = self.get_objects("h_pthf_effnew_pr", "h_pthf_effnew_np")
+            self.labels_obj = ["prompt", "nonprompt"]
+            self.title_full = f";{self.latex_pthf};efficiency"
+            self.list_latex = [self.text_alice, self.text_jets, self.get_text_range_ptjet(), self.get_text_range_pthf()]
+            self.make_plot(f"efficiency_{self.var}")
+
+            bins_ptjet = (0, 1, 2, 3)
+            self.list_obj = self.get_objects(*(f"h_ptjet-pthf_effnew_pr_"
+                                               f"{string_range_ptjet(get_bin_limits(axis_ptjet, iptjet + 1))}"
+                                               for iptjet in bins_ptjet))
+            self.labels_obj = [self.get_text_range_ptjet(iptjet) for iptjet in bins_ptjet]
+            self.make_plot(f"efficiency_ptjet_{self.var}")
+
+            # TODO: efficiency (old vs new)
 
             # loop over jet pt
             for iptjet in (1, 2):
@@ -290,84 +344,52 @@ class Plotter:
                     range_pthf = (self.edges_pthf[ipt], self.edges_pthf[ipt+1])
                     string_pthf = string_range_pthf(range_pthf)
 
-                    nameobj = f'h_ptjet-{self.var}_signal_{string_pthf}_data'
-                    h_before = self.get_object(nameobj, file_results)
-                    nameobj = f'h_ptjet-{self.var}_sideband_{string_pthf}_data'
-                    h_sideband = self.get_object(nameobj, file_results)
-                    nameobj = f'h_ptjet-{self.var}_subtracted_notscaled_{string_pthf}_data'
-                    h_after = self.get_object(nameobj, file_results)
-
-                    list_obj = [project_hist(h, [1], {0: (iptjet + 1, iptjet + 1)})
-                                for h in [h_before, h_sideband, h_after]]
-                    labels_obj = ["signal region", "scaled sidebands", "after subtraction"]
-                    colours = [get_colour(i, 1) for i in range(len(list_obj))]
-                    markers = [get_marker(i) for i in range(len(list_obj))]
-                    can, _ = make_plot(f"sidebands_{self.var}_{string_ptjet}_{string_pthf}",
-                                       list_obj=list_obj, labels_obj=labels_obj,
-                                       opt_leg_g=self.opt_leg_g, opt_plot_g=self.opt_plot_g,
-                                       offsets_xy=self.offsets_axes,
-                                       colours=colours, markers=markers, leg_pos=self.leg_pos,
-                                       margins_y=[self.y_margin_down, self.y_margin_up], margins_c=self.margins_can,
-                                       title=f";{self.title_x};counts")
-                    list_new += draw_latex_lines([self.text_alice, self.text_jets, self.get_text_range_ptjet(iptjet),
-                                                  self.get_text_range_pthf(ipt, iptjet), self.text_sd],
-                                                  y_step=self.y_step)
-                    self.save_canvas(can)
+                    self.list_obj = self.get_objects(f'h_ptjet-{self.var}_signal_{string_pthf}_data',
+                                                     f'h_ptjet-{self.var}_sideband_{string_pthf}_data',
+                                                     f'h_ptjet-{self.var}_subtracted_notscaled_{string_pthf}_data')
+                    self.list_obj = [project_hist(h, [1], {0: (iptjet + 1, iptjet + 1)}) for h in self.list_obj]
+                    self.labels_obj = ["signal region", "scaled sidebands", "after subtraction"]
+                    self.title_full = f";{self.latex_obs};counts"
+                    self.list_latex = [self.text_alice, self.text_jets, self.get_text_range_ptjet(iptjet),
+                                       self.get_text_range_pthf(ipt, iptjet)]
+                    if self.var in ("zg", "rg", "nsd"):
+                        self.list_latex.append(self.text_sd)
+                    self.make_plot(f"sidebands_{self.var}_{string_ptjet}_{string_pthf}")
 
                 # Feed-down subtraction
                 print("Plotting feed-down subtraction")
+                self.list_obj = self.get_objects(f'h_ptjet-{self.var}_{self.method}_effscaled_data',
+                                                 f'h_ptjet-{self.var}_feeddown_det_final',
+                                                 f'h_ptjet-{self.var}_{self.method}_data')
+                self.list_obj[0] = self.list_obj[0].Clone(f"{self.list_obj[0].GetName()}_fd_before_{iptjet}")
+                self.list_obj[1] = self.list_obj[1].Clone(f"{self.list_obj[1].GetName()}_fd_{iptjet}")
+                self.list_obj[2] = self.list_obj[2].Clone(f"{self.list_obj[2].GetName()}_fd_after_{iptjet}")
+                axes = list(range(get_dim(self.list_obj[0])))
+                self.list_obj = [project_hist(h, axes[1:], {0: (iptjet+1,)*2}) for h in self.list_obj]
+                self.labels_obj = ["before subtraction", "feed-down", "after subtraction"]
+                self.title_full = f";{self.latex_obs};counts"
+                self.list_latex = [self.text_alice, self.text_jets, self.get_text_range_ptjet(iptjet),
+                                   self.get_text_range_pthf(-1, iptjet)]
+                if self.var in ("zg", "rg", "nsd"):
+                    self.list_latex.append(self.text_sd)
+                self.make_plot(f"feeddown_{self.var}_{string_ptjet}")
 
-                nameobj = f'h_ptjet-{self.var}_{self.method}_effscaled_data'
-                h_before = self.get_object(nameobj, file_results)
-                h_before = h_before.Clone(f"{h_before.GetName()}_fd_before_{iptjet}")
-                nameobj = f'h_ptjet-{self.var}_feeddown_det_final'
-                h_fd = self.get_object(nameobj, file_results)
-                h_fd = h_fd.Clone(f"{h_fd.GetName()}_fd_{iptjet}")
-                nameobj = f'h_ptjet-{self.var}_{self.method}_data'
-                h_after = self.get_object(nameobj, file_results)
-                h_after = h_after.Clone(f"{h_after.GetName()}_fd_after_{iptjet}")
-
-                axes = list(range(get_dim(h_before)))
-                list_obj = [project_hist(h, axes[1:], {0: (iptjet+1,)*2}) for h in [h_before, h_fd, h_after]]
-                labels_obj = ["before subtraction", "feed-down", "after subtraction"]
-                colours = [get_colour(i, 1) for i in range(len(list_obj))]
-                markers = [get_marker(i) for i in range(len(list_obj))]
-                can, _ = make_plot(f"feeddown_{self.var}_{string_ptjet}", list_obj=list_obj, labels_obj=labels_obj,
-                                   opt_leg_g=self.opt_leg_g, opt_plot_g=self.opt_plot_g, offsets_xy=self.offsets_axes,
-                                   colours=colours, markers=markers, leg_pos=self.leg_pos,
-                                   margins_y=[self.y_margin_down, self.y_margin_up], margins_c=self.margins_can,
-                                   title=f";{self.title_x};counts")
-                list_new += draw_latex_lines([self.text_alice, self.text_jets, self.get_text_range_ptjet(iptjet),
-                                              self.get_text_range_pthf(-1, iptjet), self.text_sd], y_step=self.y_step)
-                self.save_canvas(can)
-
-                # TODO: efficiency (prompt, non-prompt, iptjet dependence, old vs new)
-                # TODO: unfolding (before/after, convergence)
                 # TODO: feed-down (after 2D, fraction)
-                # TODO: results (systematics)
+
+                # Unfolding
+                # print("Plotting unfolding")
+                # TODO: unfolding (before/after, convergence)
 
                 # Results
                 print("Plotting results")
-
-                nameobj = f"h_{self.var}_{self.method}_unfolded_data_{string_ptjet}_sel_selfnorm"
-                hf_data_stat = self.get_object(nameobj, file_results)
+                self.list_obj = [self.get_object(f"h_{self.var}_{self.method}_unfolded_data_"
+                                                 f"{string_ptjet}_sel_selfnorm")]
+                # TODO: results (systematics)
                 # nameobj = "%s_hf_data_%d_syst" % (self.var, iptjet)
                 # hf_data_syst = self.get_object(nameobj, file_results)
-
-                list_obj = [hf_data_stat]
-                labels_obj = ["data"]
-                colours = [get_colour(i, 1) for i in range(len(list_obj))]
-                markers = [get_marker(i) for i in range(len(list_obj))]
-                can, _ = make_plot(f"results_{self.var}_{string_ptjet}", list_obj=list_obj, labels_obj=labels_obj,
-                                   opt_leg_g=self.opt_leg_g, opt_plot_g=self.opt_plot_g, offsets_xy=self.offsets_axes,
-                                   colours=colours, markers=markers, leg_pos=self.leg_pos,
-                                   margins_y=[self.y_margin_down, self.y_margin_up], margins_c=self.margins_can,
-                                   title=self.title_full)
-                list_new += draw_latex_lines([self.text_alice, self.text_jets, self.get_text_range_ptjet(iptjet),
-                                              self.get_text_range_pthf(-1, iptjet), self.text_sd])
-                # gStyle.SetErrorX(0)  # do not plot horizontal error bars of histograms
-                self.save_canvas(can)
-                # gStyle.SetErrorX(0.5)  # reset default width
+                self.labels_obj = ["data"]
+                self.title_full = self.title_full_default
+                self.make_plot(f"results_{self.var}_{string_ptjet}")
 
 
 def main():
