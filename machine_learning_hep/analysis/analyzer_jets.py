f2c73b512bcd6d6a7cac513183457c2e3f0e58c5
Jochen Klein <jochen.klein@cern.ch>
2025-04-09 16:17:22 +0200

    Fix per ptjet init

--- machine_learning_hep/analysis/analyzer_jets.py
+++ machine_learning_hep/analysis/analyzer_jets.py
@@ -497,7 +497,7 @@ class AnalyzerJets(Analyzer):
             self.fit_func_bkg[level] = [None] * self.nbins
             self.fit_range[level] = [None] * self.nbins
             self.roo_ws[level] = [None] * self.nbins
-            self.roo_ws_ptjet[level] = [[None] * self.nbins] * 10
+            self.roo_ws_ptjet[level] = [[None] * self.nbins for _ in range(10)]
             rfilename = self.n_filemass_mc if "mc" in level else self.n_filemass
             fitcfg = None
             self.logger.debug("Opening file %s.", rfilename)
